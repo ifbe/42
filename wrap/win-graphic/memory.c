d779a02ab61aad1249206e4ac97a4894fdff5dfa
ifbe <ifbe@live.cn>
2015-04-03 18:04:41 +0800

    modified:   README 	renamed:    fs/ext.c -> main1/ext.c 	renamed:    fs/fat.c -> main1/fat.c 	renamed:    fs/hfs.c -> main1/hfs.c 	new file:   main1/makefile 	renamed:    fs/ntfs.c -> main1/ntfs.c 	renamed:    main/parttable.c -> main1/parttable.c 	modified:   wrap/makefile 	modified:   wrap/win-graphic/memory.c 	deleted:    wrap/win-graphic/screen.c 	renamed:    main/annoying.c -> wrap1/annoying.c 	renamed:    wrap/win-graphic/anscii.c -> wrap1/anscii.c 	renamed:    fs/makefile -> wrap1/makefile

--- wrap/win-graphic/memory.c
+++ wrap/win-graphic/memory.c
@@ -1,15 +1,24 @@
 ﻿#include<stdlib.h>
+
+int width=640;
+int height=640;
+unsigned int* mypixel;
+
 unsigned char* readbuf;		//读出来的数据存放的地址
 unsigned char* dirbuf;		//存储翻译之后的(目录名,索引号)
 unsigned char* fsbuf;		//文件系统读取代码需要的内部缓冲
+
+
 __attribute__((constructor)) void initmemory()
 {
+	mypixel=(unsigned int*)malloc(width*height*4);
 	readbuf=(unsigned char*)malloc(0x100000);
 	dirbuf=(unsigned char*)malloc(0x100000);
 	fsbuf=(unsigned char*)malloc(0x100000);
 }
 __attribute__((destructor)) void destorymemory()
 {
+	free(mypixel);
 	free(readbuf);
 	free(dirbuf);
 	free(fsbuf);
@@ -29,4 +38,12 @@ void getaddrofdir(unsigned long long* p)
 void getaddroffs(unsigned long long* p)
 {
 	*p=(unsigned long long)fsbuf;
+}
+void point(int x,int y,int color)
+{
+	mypixel[y*width+x]=color;
+}
+void draw(int x,int y,int color)
+{
+	point(x+(width/2),(height/2)-y-1,color);
 }
