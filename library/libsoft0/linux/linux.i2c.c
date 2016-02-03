eb7e4e1e917cdc59918ea82406c20debceb4a44d
ifbe <ifbe@live.cn>
2016-02-03 17:55:35 +0800

    新文件:       library/libsoft0/linux/linux.i2c.c

---
+++ library/libsoft0/linux/linux.i2c.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<linux/i2c-dev.h>
+static int fp=-1;
+static unsigned char buf[16];
+
+
+
+
+void writei2c()
+{
+}
+void readi2c(unsigned char* dest,unsigned char src)
+{
+	//write address
+	int ret=write(fp,&src,1);
+	usleep(100);
+	if(ret!=1)
+	{
+		printf("(error write)%x:%x\n",src,ret);
+		return;
+	}
+
+	//read data
+	ret=read(fp,dest,1);
+	usleep(1000);
+	if(ret!=1)
+	{
+		printf("(error read)%x:%x\n",src,ret);
+		return;
+	}
+}
+void intoi2c(char* what)	//	i2c://0x1.0x62
+{
+	//open
+	fp = open("/dev/i2c-1",O_RDWR);
+	if(fp<0)
+	{
+		printf("error open\n");
+		return;
+	}
+
+	//ioctl
+	ret=ioctl(fp,I2C_SLAVE,0x62);
+	if(ret < 0)
+	{
+		printf("error ioctl\n");
+		return;
+	}
+}
+void listi2c(char* towhere)	//	enumerate all i2c host and device
+{
+}
+
+
+
+
+void initi2c()
+{
+}
+void killi2c()
+{
+	if(fp != -1)close(fp);
+}
