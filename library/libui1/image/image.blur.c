0b1d9458bf079ae92a6eb7e2db55c9eab01eed43
ifbe <ifbe@live.cn>
2016-02-10 09:51:05 +0800

    modified:   library/libui1/image/image.blur.c

--- library/libui1/image/image.blur.c
+++ library/libui1/image/image.blur.c
@@ -1,26 +1,26 @@
 #define QWORD unsigned long long
 #define BYTE unsigned char
 //(e^(-x*x/(2*sigma^2)))/(sigma*sqrt(2*pi))
-void blur_gauss_x(QWORD size,char* addr)
+void blur_gauss_x(QWORD size,QWORD radius,char* src,char* dst)
 {
 	
 }
-void blur_gauss_y(QWORD size,char* addr)
+void blur_gauss_y(QWORD size,QWORD radius,char* src,char* dst)
 {
 	
 }
-void blur_gauss_xy(QWORD size,char* addr)
+void blur_gauss_xy(QWORD size,QWORD radius,char* src,char* dst)
 {
 	blur_gauss_x(size,addr);
 	blur_gauss_x(size,addr)
 }
 //(e^(-(x*x+y*y)/(2*sigma^2)))/(2*pi*sigma*sigma)
-void blur_gauss_2(QWORD size,char* addr)
+void blur_gauss_2(QWORD size,QWORD radius,char* src,char* dst)
 {
 	
 }
 //均值
-void blur_box(QWORD size,char* addr)
+void blur_box(QWORD size,QWORD radius,char* src,char* dst)
 {
 	
 }
