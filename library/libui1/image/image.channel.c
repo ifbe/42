a9aa0aa6e6f8daa570bf1e578c2523c5d1ae30ed
ifbe <ifbe@live.cn>
2016-02-14 19:40:49 +0800

    重命名：    library/libui1/image/image.inverse.c -> library/libsoft0/linux/linux.fakeinput.c 	新文件：    library/libsoft0/linux/linux.sql.c 	新文件：    library/libsoft0/win/win.fakeinput.c 	新文件：    library/libsoft0/win/win.sql.c 	修改：      library/libui1/image/image.blur.c 	新文件：    library/libui1/image/image.channel.c 	修改：      library/libui1/image/image.gray.c 	修改：      library/libui1/makefile

---
+++ library/libui1/image/image.channel.c
@@ -0,0 +1,66 @@
+#define QWORD unsigned long long
+#define BYTE unsigned char
+void channel_b(QWORD size,char* addr)
+{
+	int x=size&0xffff;
+	int y=(size>>16)&0xffff;
+	int i;
+	for(i=0;i<x*y;i++)
+	{
+		addr[i*4+2]=addr[i*4+1]=0;
+	}
+}
+void channel_g(QWORD size,char* addr)
+{
+	int x=size&0xffff;
+	int y=(size>>16)&0xffff;
+	int i;
+	for(i=0;i<x*y;i++)
+	{
+		addr[i*4]=addr[i*4+2]=0;
+	}
+}
+void channel_r(QWORD size,char* addr)
+{
+	int x=size&0xffff;
+	int y=(size>>16)&0xffff;
+	int i;
+	for(i=0;i<x*y;i++)
+	{
+		addr[i*4]=addr[i*4+1]=0;
+	}
+}
+
+
+
+
+void channel_rg(QWORD size,char* addr)
+{
+	int x=size&0xffff;
+	int y=(size>>16)&0xffff;
+	int i;
+	for(i=0;i<x*y;i++)
+	{
+		addr[i*4]=0;
+	}
+}
+void channel_rb(QWORD size,char* addr)
+{
+	int x=size&0xffff;
+	int y=(size>>16)&0xffff;
+	int i;
+	for(i=0;i<x*y;i++)
+	{
+		addr[i*4+1]=0;
+	}
+}
+void channel_bg(QWORD size,char* addr)
+{
+	int x=size&0xffff;
+	int y=(size>>16)&0xffff;
+	int i;
+	for(i=0;i<x*y;i++)
+	{
+		addr[i*4+2]=0;
+	}
+}
