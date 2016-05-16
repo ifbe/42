1f279ac32959dbdd6fb978d10e604ee51fe1cdfc
ifbe <ifbe@live.cn>
2016-05-16 16:26:54 +0800

    修改:         library/libsoft0/linux/linux.interface.i2c.c

--- library/libsoft0/linux/linux.interface.i2c.c
+++ library/libsoft0/linux/linux.interface.i2c.c
@@ -3,10 +3,12 @@
 #define WORD unsigned short
 #define BYTE unsigned char
 #include<stdio.h>
+#include<stdlib.h>
 #include<fcntl.h>
 #include<unistd.h>
-#include<linux/i2c-dev.h>
 #include<sys/ioctl.h>
+//#include<linux/i2c.h>		//if you have problem
+#include<linux/i2c-dev.h>
 
 
 
@@ -14,67 +16,70 @@
 //
 static int fp=-1;
 static int where[4];
-static char that[16];
+static unsigned char that[16];
+static unsigned char outbuf[16];
 
 
 
 
 int systemi2c_read(BYTE dev,BYTE reg,BYTE* buf,BYTE count)
 {
-	int ret;
+	struct i2c_msg messages[2];
+	struct i2c_rdwr_ioctl_data packets;
 
-	//select device
-	ret=ioctl(fp,I2C_SLAVE,dev);
-	if(ret < 0)
+	//out
+	outbuf[0] = reg;  
+	messages[0].addr  = dev;
+	messages[0].flags = 0;
+	messages[0].len   = 1;
+	messages[0].buf   = outbuf;
+
+	//in
+	messages[1].addr  = dev;
+	messages[1].flags = I2C_M_RD;
+	messages[1].len   = count;
+	messages[1].buf   = buf;
+
+	//send
+	packets.msgs      = messages;
+	packets.nmsgs     = 2; 
+	if(ioctl(fp, I2C_RDWR, &packets) < 0)
 	{
-		//printf("error@systemi2c_read.ioctl\n");
-		return -1;
+		//perror("Unable to send data");
+		return 1;
 	}
 
-	//select register
-	ret=write(fp,&reg,1);
-	if(ret!=1)
-	{
-		//printf("error@systemi2c_read.writereg:%x\n",ret);
-		return -2;
-	}
-
-	//read data
-	ret=read(fp,buf,count);
-	if(ret<=0)
-	{
-		//printf("error@systemi2c_read.readdata:%x\n",ret);
-		return -3;
-	}
 	return 1;
 }
 int systemi2c_write(BYTE dev,BYTE reg,BYTE* buf,BYTE count)
 {
 	int ret;
+	struct i2c_msg messages[1];
+	struct i2c_rdwr_ioctl_data packets;
 
-	//select device
-	ret=ioctl(fp,I2C_SLAVE,dev);
-	if(ret < 0)
+	//which,what0,what1,what2......
+	outbuf[0] = reg;
+	for(ret=0;ret<count;ret++)
 	{
-		//printf("error@systemi2c_write.ioctl\n");
+		outbuf[ret+1] = buf[ret];
+	}
+
+	//message
+	messages[0].addr  = dev;
+	messages[0].flags = 0;
+	messages[0].len   = count+1;
+	messages[0].buf   = outbuf;
+
+	//transfer
+	packets.msgs  = messages;
+	packets.nmsgs = 1;
+	ret=ioctl(fp, I2C_RDWR, &packets);
+	if(ret<0)
+	{
+		//perror("Unable to send data");
 		return -1;
 	}
 
-	//select register
-	ret=write(fp,&reg,1);
-	if(ret!=1)
-	{
-		//printf("error@systemi2c_write.writereg:%x\n",ret);
-		return -2;
-	}
-
-	//write data
-	ret=write(fp,buf,count);
-	if(ret<=0)
-	{
-		//printf("error@systemi2c_write.writedata:%x\n",ret);
-		return -3;
-	}
 	return 1;
 }
 
@@ -83,15 +88,15 @@ int systemi2c_write(BYTE dev,BYTE reg,BYTE* buf,BYTE count)
 
 int systemi2c_list(char* towhere)
 {
+	char ch;
 	int x;
 	int y;
 	int ret;
-	char ch;
 
 	//listbuses
 	if(where[0]==0)
 	{
-		system("ls /dev/i2c-*");
+		ret=system("ls /dev/i2c-*");
 		return 1;
 	}
 
