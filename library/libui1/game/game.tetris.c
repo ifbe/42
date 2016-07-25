da98b59204e5d87611e8c24f04f424be91694eea
ifbe <ifbe@live.cn>
2016-07-25 14:14:22 +0800

    修改:         library/libui1/game/game.tetris.c

--- library/libui1/game/game.tetris.c
+++ library/libui1/game/game.tetris.c
@@ -38,7 +38,7 @@ typedef struct stucture
 static structure that;
 
 static int score=0;
-static unsigned int* table;
+static unsigned char* table;
 
 static int width=0;
 static int height=0;
@@ -72,26 +72,10 @@ static void cubie(int x,int y,int z)
 	bodycolor=z>0?0xffffffff:0;
 
 	rect(startx+(starty<<16), endx+(endy<<16), bodycolor, 0x44444444);
-/*
-	for(i=dx*16;i<(dx+1)*16;i++){
-		for(j=y*16;j<(y+1)*16;j++){
-			screen[j*width+i]=color;
-		}
-	}
-
-	for(i=dx*16;i<dx*16+16;i++){
-		screen[i+width*(y*16)]=0x44444444;
-		screen[i+width*(y*16+16)]=0x44444444;
-	}
-	for(j=y*16;j<y*16+16;j++){
-		screen[width*j+dx*16]=0x44444444;
-		screen[width*j+dx*16+16]=0x44444444;
-	}
-*/
 }
 
 
-void generate()
+static void generate()
 {
 	if(that.type==0)
 	{
@@ -374,36 +358,91 @@ void generate()
 }
 
 
-int check()
+static int check()
 {
-	//没和已有的重合    -.    下移一格
-	int check=0;
 	unsigned int temp;
 
-	if(that.x1<0 | that.x2<0 | that.x3<0 | that.x4<0) check=1;
-	if(that.x1>31 | that.x2>31 | that.x3>31 | that.x4>31) check=1;
+	//left
+	if(that.x1<0)return 1;
+	if(that.x2<0)return 2;
+	if(that.x3<0)return 3;
+	if(that.x4<0)return 4;
 
-	temp=(table[that.y1]) & ( (unsigned int)1 << that.x1);
-	if(temp != 0)	check=1;
-	temp=(table[that.y2]) & ( (unsigned int)1 << that.x2);
-	if(temp != 0)	check=1;
-	temp=(table[that.y3]) & ( (unsigned int)1 << that.x3);
-	if(temp != 0)	check=1;
-	temp=(table[that.y4]) & ( (unsigned int)1 << that.x4);
-	if(temp != 0)	check=1;
+	//right
+	if(that.x1>31)return 5;
+	if(that.x2>31)return 6;
+	if(that.x3>31)return 7;
+	if(that.x4>31)return 8;
 
-	return check;
+	//collision
+	if(table[32 * that.y1 + that.x1] != 0)return 9;
+	if(table[32 * that.y2 + that.x2] != 0)return 10;
+	if(table[32 * that.y3 + that.x3] != 0)return 11;
+	if(table[32 * that.y4 + that.x4] != 0)return 12;
+
+	//success
+	return 0;
 }
 
 
-void time()
+static void left()
 {
+	if(that.x1>0&&that.x2>0&&that.x3>0&&that.x4>0)
+	{
+		that.x --;
+		that.x1 --;
+		that.x2 --;
+		that.x3 --;
+		that.x4 --;
+		if(check() != 0)
+		{
+			that.x ++;
+			that.x1 ++;
+			that.x2 ++;
+			that.x3 ++;
+			that.x4 ++;
+		}
+	}
+}
+static void right()
+{
+	if(that.x1<31&&that.x2<31&&that.x3<31&&that.x4<31)
+	{
+		that.x ++;
+		that.x1 ++;
+		that.x2 ++;
+		that.x3 ++;
+		that.x4 ++;
+		if(check() != 0)
+		{
+			that.x --;
+			that.x1 --;
+			that.x2 --;
+			that.x3 --;
+			that.x4 --;
+		}
+	}
+}
+static void up()
+{
+	that.direction=(that.direction +1)%4;
+	generate();
+	if(check() != 0)
+	{
+		that.direction=(that.direction+3)%4;
+	}
+	generate();
+}
+static int down()
+{
+	int x,y,count;
+
 	that.y ++;
 	that.y1 ++;
 	that.y2 ++;
 	that.y3 ++;
 	that.y4 ++;
-	if(check() == 0)return;
+	if(check() < 9)return 0;
 
 	that.y --;
 	that.y1 --;
@@ -411,99 +450,75 @@ void time()
 	that.y3 --;
 	that.y4 --;
 
-	//老的方块融入世界
-	table[that.y1] |= ( (unsigned int)1 << that.x1);
-	table[that.y2] |= ( (unsigned int)1 << that.x2);
-	table[that.y3] |= ( (unsigned int)1 << that.x3);
-	table[that.y4] |= ( (unsigned int)1 << that.x4);
+	table[32 * that.y1 + that.x1]=1;
+	table[32 * that.y2 + that.x2]=1;
+	table[32 * that.y3 + that.x3]=1;
+	table[32 * that.y4 + that.x4]=1;
 
-	//新的方块们
+	y=39;
+	while(1)
+	{
+		count=0;
+		for(x=0;x<32;x++)
+		{
+			if(table[32*y+x]>0)count++;
+		}
+
+		if(count==32)
+		{
+			//
+			for(x=y*32+31;x>32;x--)
+			{
+				table[x]=table[x-32];
+			}
+		}
+
+		else
+		{
+			y--;
+			if(y==0)break;
+		}
+	}
+
+	//
 	that.x=getrandom() %27+1;
 	that.y=1;
 	that.type=getrandom() % 7;
 	that.direction=getrandom() & 0x3;
 	generate();
-}
-void kbd(int key)
-{
-	if(key=='s'|key==0x28)
-	{
-		time();
-	}
-	else if(key=='a'|key==0x25)
-	{
-		if(that.x1>0&&that.x2>0&&that.x3>0&&that.x4>0)
-		{
-			that.x --;
-			that.x1 --;
-			that.x2 --;
-			that.x3 --;
-			that.x4 --;
-			if(check() != 0)
-			{
-				that.x ++;
-				that.x1 ++;
-				that.x2 ++;
-				that.x3 ++;
-				that.x4 ++;
-			}
-		}
-	}
-	else if(key=='d'|key==0x27)
-	{
-		if(that.x1<31&&that.x2<31&&that.x3<31&&that.x4<31)
-		{
-			that.x ++;
-			that.x1 ++;
-			that.x2 ++;
-			that.x3 ++;
-			that.x4 ++;
-			if(check() != 0)
-			{
-				that.x --;
-				that.x1 --;
-				that.x2 --;
-				that.x3 --;
-				that.x4 --;
-			}
-		}
-	}
-	else if(key=='w' | key==' '|key==0x26)		//翻转
-	{
-		that.direction=(that.direction +1)%4;
-		generate();
-		if(check(table,&that) != 0)
-		{
-			that.direction=(that.direction+3)%4;
-		}
-		generate();
-	}
+	return 1;
 }
 void tetris_write(QWORD type,QWORD key)
 {
-	int i,j;
-
-	if(type==0x6b636f746b636974)	//ticktock
+	int ret;
+	if(type==0x6b6c63)
 	{
-		time();
+		down();
 	}
 	else if(type==0x64626b)
 	{
-		kbd(key);
-	}
-
-	//one line full ?
-	for(i=39;i>0;i--)
-	{
-		if(table[i] == 0xffffffff)
+		if(key=='a'|key==0x25)
 		{
-			for(j=i;j>0;j--)
-			{
-				table[j]=table[j-1];
-			}
-			table[0]=0;
-
-			score++;
+			left();
+		}
+		else if(key=='d'|key==0x27)
+		{
+			right();
+		}
+		else if(key=='w' | key==' '|key==0x26)
+		{
+			up();
+		}
+		else if(key=='s'|key==0x28)
+		{
+			if(down()==1)return;
+			if(down()==1)return;
+			if(down()==1)return;
+			if(down()==1)return;
+			if(down()==1)return;
+			if(down()==1)return;
+			if(down()==1)return;
+			if(down()==1)return;
 		}
 	}
 }
@@ -513,29 +528,39 @@ void tetris_write(QWORD type,QWORD key)
 
 void tetris_read()
 {
-	int i,j;
-	unsigned int temp;
+	int x,y;
 
-	for(j=0;j<40;j++)
+	for(y=0;y<40;y++)
 	{
-		temp=table[j];
-		for(i=0;i<32;i++)
+		for(x=0;x<32;x++)
 		{
-			cubie(i,j,temp & 0x1);
-			temp/=2;    //temp>>1 wrong,do not know why
+			//say("%d ",table[y*32+x]);
+			cubie(x,y,table[y*32+x]);
 		}
+		//say("\n");
 	}
+	//say("\n");
+
 	//print cubies
 	cubie(that.x1,that.y1,1);
 	cubie(that.x2,that.y2,1);
 	cubie(that.x3,that.y3,1);
 	cubie(that.x4,that.y4,1);
+
 	//print score
 	//decimal(10,10,score);
 }
+
+
+
+
 void tetris_list()
 {
 }
+
+
+
+
 void tetris_choose()
 {
 }
@@ -553,7 +578,6 @@ static void tetris_start(DWORD size,void* addr)
 	width=size&0xffff;
 	height=(size>>16)&0xffff;
 	screen=(unsigned int*)addr;
-	table=(unsigned int*)(addr+0x300000);
 
 	//
 	ascii_start(size,addr);
@@ -567,8 +591,9 @@ static void tetris_start(DWORD size,void* addr)
 		screen[x]=0;
 	}
 
-	for(x=0;x<40;x++) table[x]=0;
-	table[40]=0xffffffff;
+	for(x= 0*32;x<40*32;x++) table[x]=0;
+	for(x=40*32;x<41*32;x++) table[x]=1;
+
 	that.x=getrandom() %27 +1;
 	that.y=1;
 	that.type=5;
@@ -590,6 +615,8 @@ void tetris_init(char* base,void* addr)
 	this[5]=(QWORD)tetris_choose;
 	this[6]=(QWORD)tetris_read;
 	this[7]=(QWORD)tetris_write;
+
+	table=(unsigned char*)(addr+0x300000);
 }
 void tetris_kill()
 {
