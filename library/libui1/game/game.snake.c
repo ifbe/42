0ef5b2ebf9a39e24f02e5018ca7451267726bb5a
ifbe <ifbe@live.cn>
2016-08-02 13:25:16 +0800

    修改:         library/libui1/game/game.snake.c

--- library/libui1/game/game.snake.c
+++ library/libui1/game/game.snake.c
@@ -3,6 +3,8 @@
 //
 DWORD getrandom();
 void rect(DWORD p1,DWORD p2,DWORD bodycolor,DWORD framecolor);
+void rectbody(DWORD p1,DWORD p2,DWORD bodycolor);
+void rectframe(DWORD p1,DWORD p2,DWORD bodycolor);
 //
 void background_start(DWORD size,void* addr);
 void shape_start(DWORD size,void* addr);
@@ -14,22 +16,23 @@ void say(char*,...);
 
 
 
-struct snake{
+struct hehe{
 	int x;
 	int y;
-	int direction;
 };
-static struct snake* snake;
-static int length;
+static struct hehe* snake;
+static int len;
+//
+static struct hehe a;
+static struct hehe b;
 //
 static int foodx;
 static int foody;
 //
-static int score=0;
-static int die=0;
-//
 static int width;
 static int height;
+static int score=0;
+static int die=0;
 
 
 
@@ -41,26 +44,26 @@ void snake_read()
 	if(die == 1)return;
 
 	//
-	rect(
-		(16*snake[length-1].x - 16) + ((16*snake[length-1].y - 16)<<16),
-		(16*snake[length-1].x + 31) + ((16*snake[length-1].y + 31)<<16),
-		0,
-		0
+	rectbody(
+		(16*a.x) + ((16*a.y)<<16),
+		(16*a.x + 15) + ((16*a.y + 15)<<16),
+		0xf
 	);
 
 
 	//
 	j=0;
-	while( snake[j].direction != 0 )
+	while(1)
 	{
-say("%d:%d,%d,%d\n",j,snake[j].x,snake[j].y,snake[j].direction);
 		rect(
 			(16*snake[j].x) + ((16*snake[j].y)<<16),
 			(16*snake[j].x + 15) + ((16*snake[j].y + 15)<<16),
 			0xffffffff,
 			0
 		);
+
 		j++;
+		if(j>len)break;
 	}
 
 	//
@@ -82,45 +85,34 @@ void snake_write(QWORD type,QWORD key)
 
 	if(type==0x64626b)
 	{
-		snake[length].x=snake[length-1].x;
-		snake[length].y=snake[length-1].y;
-		snake[length].direction=snake[length-1].direction;
-		snake[1].direction=snake[0].direction;
-
 		if(key=='a'|key==0x25)
 		{
-			snake[0].direction=1;
+			a.x=snake[0].x;
+			a.y=snake[0].y;
 			snake[0].x--;
 		}
 		else if(key=='d'|key==0x27)
 		{
-			snake[0].direction=2;
+			a.x=snake[0].x;
+			a.y=snake[0].y;
 			snake[0].x++;
 		}
 		else if(key=='w'|key==0x26)
 		{
-			snake[0].direction=3;
+			a.x=snake[0].x;
+			a.y=snake[0].y;
 			snake[0].y--;
 		}
 		else if(key=='s'|key==0x28)
 		{
-			snake[0].direction=4;
+			a.x=snake[0].x;
+			a.y=snake[0].y;
 			snake[0].y++;
 		}
+		else return;
 	}
 	else return;
-/*
-	else
-	{
-		switch(snake[0].direction)
-		{
-			case 1:snake[0].x--; break;
-			case 2:snake[0].x++; break;
-			case 3:snake[0].y--; break;
-			case 4:snake[0].y++; break;
-		}
-	}
-*/
+
 	//撞墙
 	if(	(snake[0].x < 0) |
 		(snake[0].x > (width/16)) |
@@ -131,51 +123,37 @@ void snake_write(QWORD type,QWORD key)
 		return;
 	}
 
+	//蛇身往前挪动
+	j=0;
+	while(1)
+	{
+		j++;
+		if(j >= len)break;
+
+		b.x = snake[j].x;
+		b.y = snake[j].y;
+		snake[j].x = a.x;
+		snake[j].y = a.y;
+		a.x = b.x;
+		a.y = b.y;
+
+		if( (snake[j].x==snake[0].x) && (snake[j].y==snake[0].y) )
+		{
+			die=1;
+			return;
+		}
+	}
+
 	//吃到食物
 	if(snake[0].x==foodx && snake[0].y==foody)
 	{
+		snake[j].x = a.x;
+		snake[j].y = a.y;
+		len++;
+
 		foodx=getrandom() % (width/16);
 		foody=getrandom() % (height/16);
-
-		length++;
 	}
-
-	//蛇身往前挪动
-	j=1;
-	while(1)
-	{
-		if(j >= length)
-		{
-			snake[j].direction = 0;
-			break;
-		}
-
-		if(snake[j].direction == 1)
-		{
-			snake[j].x--;
-		}
-		else if(snake[j].direction == 2)
-		{
-			snake[j].x++;
-		}
-		else if(snake[j].direction == 3)
-		{
-			snake[j].y--;
-		}
-		else if(snake[j].direction == 4)
-		{
-			snake[j].y++;
-		}
-/*
-		if(snake[0].x==snake[j].x && snake[0].y==snake[j].y)
-		{
-			die=1;
-		}
-*/
-		snake[j].direction = snake[j-1].direction;
-		j++;
-	}
-
 }
 
 
@@ -188,14 +166,12 @@ static void snake_choose()
 {
 	//init food and snake 
 	int i;
-	for(i=1;i<100;i++) snake[i].direction=0;
 	snake[0].x=1 + getrandom() % ((width/16)-2);
 	snake[0].y=1 + getrandom() % ((height/16)-2);
-	snake[0].direction=1;
-	snake[1].x=snake[0].x+1;
-	snake[1].y=snake[0].y;
-	snake[1].direction=1;
-	length=2;
+	len=1;
+
+	a.x=0;
+	b.x=0;
 
 	foodx=getrandom() % (width/16);
 	foody=getrandom() % (height/16);
@@ -247,7 +223,7 @@ void snake_init(char* base,void* addr)
 	this[6]=(QWORD)snake_read;
 	this[7]=(QWORD)snake_write;
 
-	snake=(struct snake*)(addr+0x300000);
+	snake=(struct hehe*)(addr+0x300000);
 }
 void snake_kill()
 {
