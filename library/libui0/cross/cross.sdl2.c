2e3302afa024e28427a98f75037df2b0ffc393cd
ifbe <ifbe@live.cn>
2016-09-16 23:29:05 +0800

    modified:   app/none+x64+win+sdl/makefile 	modified:   library/libui0/cross/cross.sdl2.c 	modified:   library/libui0/makefile

--- library/libui0/cross/cross.sdl2.c
+++ library/libui0/cross/cross.sdl2.c
@@ -1,6 +1,7 @@
+#include<stdio.h>
 #include<stdlib.h>
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_video.h"
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_video.h>
 #undef main
 //
 #define u64 unsigned long long
@@ -18,33 +19,13 @@ SDL_Texture* texture;		//texture是在显存里的？
 SDL_TimerID my_timer_id;
 
 
-void windowcreate(char* addr)
+void windowcreate()
 {
-	//准备rgb点阵
-	mypixel=(unsigned int*)addr;
-
 	//准备sdl
 	SDL_Init(SDL_INIT_EVERYTHING);
-	window=SDL_CreateWindow("i am groot!",
-				SDL_WINDOWPOS_UNDEFINED,
-				SDL_WINDOWPOS_UNDEFINED,
-				width,height,
-				SDL_WINDOW_OPENGL);
-				renderer= SDL_CreateRenderer(window, -1, 0);
-	texture= SDL_CreateTexture(renderer,
-				SDL_PIXELFORMAT_ARGB8888,
-				SDL_TEXTUREACCESS_STREAMING,
-				width,height);
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-
 }
 void windowdelete()
 {
-	//释放点阵
-	free(mypixel);
-
 	//释放sdl
 	SDL_DestroyTexture(texture);
 	SDL_DestroyRenderer(renderer);
@@ -53,6 +34,26 @@ void windowdelete()
 }
 void windowstart(char* addr, char* pixfmt, int x, int y)
 {
+	//准备rgb点阵
+	mypixel = (unsigned int*)addr;
+	width = x;
+	height = y;
+
+	//
+	window=SDL_CreateWindow("i am groot!",
+				SDL_WINDOWPOS_UNDEFINED,
+				SDL_WINDOWPOS_UNDEFINED,
+				width,height,
+				SDL_WINDOW_OPENGL);
+
+	renderer= SDL_CreateRenderer(window, -1, 0);
+	texture= SDL_CreateTexture(renderer,
+				SDL_PIXELFORMAT_ARGB8888,
+				SDL_TEXTUREACCESS_STREAMING,
+				width,height);
+	//SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	//SDL_RenderClear(renderer);
+	//SDL_RenderPresent(renderer);
 }
 void windowstop()
 {
@@ -85,6 +86,7 @@ void windowwrite()
 int uievent(u64* who, u64* type, u64* value)
 {
 	SDL_Event event;
+	int val;
 	while (1)
 	{
 		if (SDL_WaitEvent(&event))
@@ -96,9 +98,21 @@ int uievent(u64* who, u64* type, u64* value)
 			}
 			else if (event.type == SDL_KEYDOWN)
 			{
-				*type=1;
-				*value=(int)(event.key.keysym.sym);
-				return 1;
+				*type=0x64626b;
+				val = event.key.keysym.sym;
+				//printf("%x\n",val);
+
+				if(val==0x1b){*value=0x1b;return 0;}
+				else if(val==0x40000050){*value=0x25;return 0;}
+				else if(val==0x40000052){*value=0x26;return 0;}
+				else if(val==0x4000004f){*value=0x27;return 0;}
+				else if(val==0x40000051){*value=0x28;return 0;}
+				else
+				{
+					*type = 0x72616863;
+					*value = val;
+					return 1;
+				}
 			}
 			else if(event.type == SDL_MOUSEBUTTONDOWN)	//MOUSEMOTION
 			{
@@ -108,7 +122,7 @@ int uievent(u64* who, u64* type, u64* value)
 					int x=event.button.x;
 					int y=event.button.y;
 
-					*type=2;
+					*type=0x7466656C207A7978;
 					*value=x+(y<<16);
 					return 1;
 				}
