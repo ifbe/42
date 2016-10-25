31374d83bad898442aaac8b8a820b1803b31d0c2
ifbe <ifbe@live.cn>
2016-10-25 17:22:08 +0800

    修改：     library/libboot1/debug.c 	修改：     library/libsoft0/linux/linux.net.client.c

--- library/libboot1/debug.c
+++ library/libboot1/debug.c
@@ -9,43 +9,52 @@ void say(char*,...);
 
 void printmemory(u8* addr,int size)
 {
-//debug用，打印从addr开始的总共size个字节
-	int i,j;
+	int x,y;
+	u8 ch;
+	u8* p;
 
-	//111111111111111111111111
-	if(size<0x10)
+	for(y=0;y<size/16;y++)
 	{
-		for(i=0;i<size;i++)say("%.2x ",addr[i]);
-		say("\n");
-		return;
-	}
+		p = addr+y*16;
 
-	//2222222222222222222222222
-	say("[----addr----]");
-	for(i=0;i<=0xf;i++)
-	{
-		say("%.2x ",i);
-	}
-	say("[----anscii----]\n");
+		//addr
+		if((y%16) == 0)say( "@%-13llx" , (u64)p );
+		else say("+%-13x",y*16);
 
-	//33333333333333333333333333
-	for(j=0;j<size/16;j++)
-	{
-		if(j%16 == 0)say( "@%-13llx" , (u64)(addr+j*16) );
-		else say("+%-13x",j*16);
-
-		for(i=0;i<=0xf;i++)
+		//hex
+		for(x=0;x<=0xf;x++)
 		{
-			say("%.2x ",addr[16*j+i]);
+			say("%02x ",p[x]);
 		}
-		for(i=0;i<=0xf;i++)
+
+		//ascii
+		for(x=0;x<=0xf;x++)
 		{
-			u8 ch=addr[16*j+i];
-			if( (ch>=0x80)|(ch<=0x20) )ch=0x20;
+			ch = p[x];
+			if( (ch>=0x7f)|(ch<=0x20) )ch=0x20;
 			say("%c",ch);
 		}
 		say("\n");
 	}
+
+	p = addr+y*16;
+	say("+%-13x",y*16);
+	for(x=0;x<16;x++)
+	{
+		if(x >= size%16)say("   ");
+		else say("%02x ",p[x]);
+	}
+	for(x=0;x<16;x++)
+	{
+		if(x >= size%16)say(" ");
+		else
+		{
+			ch = p[x];
+			if( (ch>=0x7f)|(ch<=0x20) )ch=0x20;
+			say("%c",ch);
+		}
+	}
+	say("\n\n");
 }
 
 
--- library/libsoft0/linux/linux.net.client.c
+++ library/libsoft0/linux/linux.net.client.c
@@ -3,13 +3,21 @@
 #include<string.h>
 #include<errno.h>
 #include<fcntl.h>
-#include<signal.h>
 #include<unistd.h>
 #include<pthread.h>
+#include<signal.h>
+#include<arpa/inet.h>
+#include<net/if.h>
+#include<netinet/in.h>
+#include<netinet/ether.h>
+#include<sys/ioctl.h>
 #include<sys/socket.h>
 #include<sys/types.h>
-#include<arpa/inet.h>
-#include<netinet/in.h>
+#define u8 unsigned char
+#define u16 unsigned short
+#define u32 unsigned int
+#define u64 unsigned long long
+void printmemory(char*,int);
 void say(char* , ...);
 
 
@@ -29,14 +37,23 @@ static struct sockaddr_in server = {0};
 
 void* readclient(void* p)
 {
+	int j;
 	int ret;
-	char buf[256];
+	u8 buf[0x1000];
 
-	if(st == SOCK_DGRAM)
+	if(st == SOCK_RAW)
 	{
 		while(alive == 1)
 		{
-			ret=recvfrom(fd, buf, 256, 0, (void*)&server, (void*)&serlen);
+			ret=recvfrom(fd, buf, 0x1000, 0, NULL, NULL);
+			printmemory(buf,ret);
+		}
+	}
+	else if(st == SOCK_DGRAM)
+	{
+		while(alive == 1)
+		{
+			ret=recvfrom(fd, buf, 0x1000, 0, (void*)&server, (void*)&serlen);
 			if(ret<0)break;
 
 			buf[ret] = 0;
@@ -48,7 +65,7 @@ void* readclient(void* p)
 	{
 		while(alive == 1)
 		{
-			ret=read(fd, buf, 256);
+			ret=read(fd, buf, 0x1000);
 			if(ret<0)break;
 
 			buf[ret] = 0;
@@ -58,7 +75,7 @@ void* readclient(void* p)
 	}
 	return 0;
 }
-int writeclient(char* buf,int len)
+int writeclient(u8* buf,int len)
 {
 	int ret;
 	if(st == SOCK_DGRAM)
@@ -92,6 +109,7 @@ int listclient(char* type)
 int chooseclient(char* type, char* addr, int port, char* extra)
 {
 	int ret;
+
 	if(fd > 0)
 	{
 		alive = 0;
@@ -102,15 +120,51 @@ int chooseclient(char* type, char* addr, int port, char* extra)
 	if(addr == 0)return 0;
 	if(addr[0] == 0)return 0;
 
-	//create struct
-	memset(&server, 0, sizeof(struct sockaddr_in));
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr(addr);
-	server.sin_port = htons(port);
-	serlen = sizeof(struct sockaddr_in);
-
-	if(strncmp(type, "udp", 3) == 0)
+	if(strncmp(type, "raw", 3) == 0)
 	{
+		int sockopt;
+		struct ifreq ifopts;
+		char* ifName = "eth0";
+
+		fd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+		if(fd == -1)
+		{
+			printf("error%d@socket:%d\n",errno,fd);
+			return 0;
+		}
+
+		strncpy(ifopts.ifr_name, ifName, 5);
+		ioctl(fd, SIOCGIFFLAGS, &ifopts);
+		ifopts.ifr_flags |= IFF_PROMISC;
+		ioctl(fd, SIOCSIFFLAGS, &ifopts);
+
+		ret = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof sockopt);
+		if(ret == -1)
+		{
+			perror("setsockopt");
+			close(fd);
+			exit(EXIT_FAILURE);
+		}
+
+		ret = setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, ifName, 5);
+		if(ret == -1)
+		{
+			perror("SO_BINDTODEVICE");
+			close(fd);
+			exit(EXIT_FAILURE);
+		}
+
+		st = SOCK_RAW;
+	}
+	else if(strncmp(type, "udp", 3) == 0)
+	{
+		//create struct
+		memset(&server, 0, sizeof(struct sockaddr_in));
+		server.sin_family = AF_INET;
+		server.sin_addr.s_addr = inet_addr(addr);
+		server.sin_port = htons(port);
+		serlen = sizeof(struct sockaddr_in);
+
 		//create socket
 		fd = socket(AF_INET, SOCK_DGRAM, 0);
 		if(fd == -1)
@@ -123,6 +177,13 @@ int chooseclient(char* type, char* addr, int port, char* extra)
 	}
 	else
 	{
+		//create struct
+		memset(&server, 0, sizeof(struct sockaddr_in));
+		server.sin_family = AF_INET;
+		server.sin_addr.s_addr = inet_addr(addr);
+		server.sin_port = htons(port);
+		serlen = sizeof(struct sockaddr_in);
+
 		//create socket
 		fd = socket(AF_INET, SOCK_STREAM, 0);
 		if(fd == -1)
