0699de9f7bf3279fbb070dca964d27846f9f32c6
ifbe <ifbe@live.cn>
2016-11-30 11:39:56 +0800

    修改：     library/libui1/makefile 	修改：     library/libui1/test/test.example.c 	新文件：   library/libui1/tool/tool.kirchhoff.c

--- library/libui1/test/test.example.c
+++ library/libui1/test/test.example.c
@@ -8,8 +8,10 @@
 
 
 
-#define u64 unsigned long long
+#define u8 unsigned char
+#define u16 unsigned short
 #define u32 unsigned int
+#define u64 unsigned long long
 //
 void backgroundcolor();
 //
