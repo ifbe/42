8421ecba8c76ab50ed5b9817aae07c1d59cae080
ifbe <ifbe@live.cn>
2016-12-08 22:26:22 +0800

    getdate(world,century,year,month,day,hour,minute,second)

--- library/libsoft0/linux/linux.system.time.c
+++ library/libsoft0/linux/linux.system.time.c
@@ -1,15 +1,57 @@
+#include<stdio.h>
 #include<unistd.h>
 #include<time.h>
 #include<sys/time.h>
+#define u8 unsigned char
 #define u64 unsigned long long
 
-u64 gettime()
-{
-	struct timeval t;
-	gettimeofday(&t,0);
-	return (t.tv_sec)*1000*1000 + (t.tv_usec);
-}
+
+
+
 void sleep_us(int t)
 {
 	usleep(t);
+}
+u64 since1970()
+{
+	return time(0);
+}
+
+
+
+
+u64 gettime()
+{
+        struct timeval t;
+        gettimeofday(&t,0);
+        return (t.tv_sec)*1000*1000 + (t.tv_usec);
+}
+u64 getdate()
+{
+	u64 temp;
+	time_t timep;
+	struct tm *p;
+	time(&timep);
+	p = gmtime(&timep);
+
+	//byte7=world
+
+	//byte6=centry, byte5=year
+	temp = 1900 + (p->tm_year);
+	temp = ((temp/100)<<8) + (temp%100);
+
+	//byte4=month
+	temp = (temp<<8) + 1 + (p->tm_mon);
+
+	//byte3=day
+	temp = (temp<<8) + (p->tm_mday);
+
+	//byte2=hour
+	temp = (temp<<8) + (p->tm_hour);
+
+	//byte1=minute
+	temp = (temp<<8) + (p->tm_min);
+
+	//byte0=second
+	temp = (temp<<8) + (p->tm_sec);
 }
--- library/libsoft0/win/win.system.time.c
+++ library/libsoft0/win/win.system.time.c
@@ -1,11 +1,55 @@
+#include<stdio.h>
+#include<time.h>
 #include<windows.h>
+#define u8 unsigned char
 #define u64 unsigned long long
 u64 GetTickCount64();
+
+
+
+
+void sleep_us(int t)
+{
+	Sleep(t/1000);
+}
+u64 since1970()
+{
+	return time(0);
+}
+
+
+
+
 u64 gettime()
 {
 	return 1000 * GetTickCount64();
 }
-void sleep_us(int t)
+u64 getdate()
 {
-	Sleep(t/1000);
+	u64 temp;
+	time_t timep;
+	struct tm *p;
+	time(&timep);
+	p = gmtime(&timep);
+
+	//byte7=world
+
+	//byte6=centry, byte5=year
+	temp = 1900 + (p->tm_year);
+	temp = ((temp/100)<<8) + (temp%100);
+
+	//byte4=month
+	temp = (temp<<8) + 1 + (p->tm_mon);
+
+	//byte3=day
+	temp = (temp<<8) + (p->tm_mday);
+
+	//byte2=hour
+	temp = (temp<<8) + (p->tm_hour);
+
+	//byte1=minute
+	temp = (temp<<8) + (p->tm_min);
+
+	//byte0=second
+	temp = (temp<<8) + (p->tm_sec);
 }
