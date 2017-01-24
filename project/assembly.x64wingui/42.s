b7d383438fde2ad2bdad25115bbceb20da5f4281
ifbe <ifbe@live.cn>
2017-01-24 09:08:53 +0800

    assembly

--- project/assembly.x64wingui/42.s
+++ project/assembly.x64wingui/42.s
@@ -1,18 +1,17 @@
 [bits 64]
 extern birth
 extern windowstart
-extern characterstart
 
 extern eventread
 extern eventwrite
 extern windowread
 extern windowwrite
-extern characterread
-extern characterwrite
 
-extern characterstop
 extern windowstop
 extern death
+
+
+
 
 section .text
 global main
@@ -25,14 +24,8 @@ main:
 	mov r9,512
 	call windowstart
 
-	mov rcx,pixbuf
-	mov rdx,pixfmt
-	mov r8,512
-	mov r9,512
-	call characterstart
-
 mainloop:
-	call characterread
+	call aaaaaaaa
 	call windowwrite
 
 eventloop:
@@ -41,20 +34,32 @@ eventloop:
 	je bye
 	cmp dword [rax+8],0
 	je bye
-	cmp dword [rax+8],0x656d6974
-	je eventloop
 
-	mov rcx,rax
-	call characterwrite
-
+	call bbbbbbbb
 	jmp mainloop
 
 bye:
 	call death
 	ret
 
+aaaaaaaa:
+	mov rdi,pixbuf
+	mov rax,[color]
+	mov rcx,512*512
+	rep stosd
+	ret
+
+bbbbbbbb:
+	inc qword [color]
+	ret
+
+
+
+
 section .data
 pixbuf:
 	times (2048*1024*4)+0x100000 db 0
 pixfmt:
 	db 'b','g','r','a','8','8','8','8'
+color:
+	dd 0
