fedbfc4a1ecd136bc16aa08c847cb241de41c19a
ifbe <ifbe@live.cn>
2017-07-20 18:31:16 +0800

    hash

--- library/libsoft1/libmem/libdata/hash/hash.string.c
+++ library/libsoft1/libmem/libdata/hash/hash.string.c
@@ -1,101 +1,114 @@
-unsigned int bkdrhash(char* str,int i)
+#define u8 unsigned char
+#define u16 unsigned short
+#define u32 unsigned int
+#define u64 unsigned long long
+
+
+
+
+u32 bkdrhash(u8* buf, int len)
 {
-	unsigned int seed = 131; // 31 131 1313 13131 131313 etc..
-	unsigned int hash = 0;
-	while (*str)
+	int j;
+	u32 hash = 0;
+	for(j=0;j<len;j++)
 	{
-		hash = hash * seed + (*str++);
+		hash = hash * 131 + buf[j];
 	}
-	return (hash & 0x7FFFFFFF);
+	return hash;
 }
-unsigned int aphash(char* str,int i)
+u32 aphash(u8* buf, int len)
 {
-	unsigned int hash = 0;
-	for (i=0; *str; i++)
+	int j;
+	u32 hash = 0;
+	for(j=0;j<len;j++)
 	{
-		if ((i & 1) == 0)
+		if ((j & 1) == 0)
 		{
-			hash ^= ((hash << 7) ^ (*str++) ^ (hash >> 3));
+			hash ^= ((hash << 7) ^ buf[j] ^ (hash >> 3));
 		}
 		else
 		{
-			hash ^= (~((hash << 11) ^ (*str++) ^ (hash >> 5)));
+			hash ^= (~((hash << 11) ^ buf[j] ^ (hash >> 5)));
 		}
 	}
-	return (hash & 0x7FFFFFFF);
+	return hash;
 }
-unsigned int djb2hash(char* str,int i)
+u32 djb2hash(u8* buf, int len)
 {
-	unsigned int hash = 5381;
-	while (*str)
+	int j;
+	u32 hash = 5381;
+	for(j=0;j<len;j++)
 	{
-		hash += (hash << 5) + (hash) +(*str);
-		str++;
+		hash += (hash<<5) + hash + buf[j];
 	}
-	return (hash & 0x7FFFFFFF);
+	return hash;
 }
-unsigned int jshash(char* str,int i)
+u32 jshash(u8* buf, int len)
 {
-	unsigned int hash = 1315423911;
-	while (*str)
+	int j;
+	u32 hash = 1315423911;
+	for(j=0;j<len;j++)
 	{
-		hash ^= ((hash << 5) + (*str++) + (hash >> 2));
+		hash ^= (hash << 5) + (hash >> 2) + buf[j];
 	}
-	return (hash & 0x7FFFFFFF);
+	return hash;
 }
-unsigned int rshash(char* str,int i)
+u32 rshash(u8* buf, int len)
 {
-	unsigned int b = 378551;
-	unsigned int a = 63689;
-	unsigned int hash = 0;
-	while (*str)
+	int j;
+	u32 a = 63689;
+	u32 b = 378551;
+	u32 hash = 0;
+	for(j=0;j<len;j++)
 	{
-		hash = hash * a + (*str++);
+		hash = hash * a + buf[j];
 		a *= b;
 	}
-	return (hash & 0x7FFFFFFF);
+	return hash;
 }
-unsigned int sdbmhash(char* str,int i)
+u32 sdbmhash(u8* buf, int len)
 {
-	unsigned int hash = 0;
-	while (*str)
+	int j;
+	u32 hash = 0;
+	for(j=0;j<len;j++)
 	{
 		// equivalent to: hash = 65599*hash + (*str++);
-		hash = (*str++) + (hash << 6) + (hash << 16) - hash;
+		hash = buf[j] + (hash << 6) + (hash << 16) - hash;
 	}
-	return (hash & 0x7FFFFFFF);
+	return hash;
 }
-unsigned int pjwhash(char* str,int i)
+u32 pjwhash(u8* buf, int len)
 {
-	unsigned int BitsInUnignedInt = (unsigned int)(sizeof(unsigned int) * 8);
-	unsigned int ThreeQuarters    = (unsigned int)((BitsInUnignedInt  * 3) / 4);
-	unsigned int OneEighth        = (unsigned int)(BitsInUnignedInt / 8);
-	unsigned int HighBits         = (unsigned int)(0xFFFFFFFF) << (BitsInUnignedInt - OneEighth);
-	unsigned int hash             = 0;
-	unsigned int test             = 0;
-
-	while (*str)
+	u32 BitsInUnignedInt = (u32)(sizeof(u32) * 8);
+	u32 ThreeQuarters    = (u32)(BitsInUnignedInt * 3 / 4);
+	u32 OneEighth        = (u32)(BitsInUnignedInt / 8);
+	u32 HighBits         = (u32)(0xFFFFFFFF) << (BitsInUnignedInt - OneEighth);
+	u32 hash             = 0;
+	u32 test             = 0;
+	int j;
+	for(j=0;j<len;j++)
 	{
-		hash = (hash << OneEighth) + (*str++);
+		hash = (hash << OneEighth) + buf[j];
 		if ((test = hash & HighBits) != 0)
 		{
 			hash = ((hash ^ (test >> ThreeQuarters)) & (~HighBits));
 		}
 	}
-	return (hash & 0x7FFFFFFF);
+	return hash;
 }
-unsigned int elfhash(char* str,int i)
+u32 elfhash(u8* buf, int len)
 {
-	unsigned int hash = 0;
-	unsigned int x    = 0;
-	while (*str)
+	int j;
+	u32 x = 0;
+	u32 hash = 0;
+	for(j=0;j<len;j++)
 	{
-		hash = (hash << 4) + (*str++);
+		hash = (hash << 4) + buf[j];
 		if ((x = hash & 0xF0000000L) != 0)
 		{
 			hash ^= (x >> 24);
 			hash &= ~x;
 		}
 	}
-	return (hash & 0x7FFFFFFF);
+	return hash;
 }
