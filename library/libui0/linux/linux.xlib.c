a2d2d42a7fe4fbb4d74e0131f09826b6e7fca2c4
ifbe <ifbe@live.cn>
2017-10-26 19:19:06 +0800

    xlib

--- library/libui0/linux/linux.xlib.c
+++ library/libui0/linux/linux.xlib.c
@@ -69,21 +69,22 @@ static u8 xlib2kbd[0x80]={
 0x00,0x00,0x00,0x00,	0x00,0x00,0x00,0x00,	//78,7f
 };
 //
-static Display* dsp=0;
-static Visual* visual=0;
+static Display* dsp = 0;
+static Visual* visual = 0;
+static Window root = 0;
 static Atom wmDelete;
+static Atom wmCreate;
 //
 static struct window* palette;
+static struct window* theone;
+static void* maptab[0x1000];
 
 
 
 
-void* uievent(struct window* this)
+static int fuckyou=0;
+void createmywindow(struct window* this)
 {
-	XEvent ev;
-	u64 x,y,k;
-	u64 why, what, where;
-
 	//window, gc
 	this->fd = XCreateSimpleWindow(
 		dsp, RootWindow(dsp,0), 0, 0,
@@ -107,174 +108,176 @@ void* uievent(struct window* this)
 
 	//
 	XMapWindow(dsp, this->fd);
+	maptab[(this->fd) & 0xfff] = this;
+	fuckyou++;
+}
+void* uievent(struct window* this)
+{
+	u64 x,y,k;
+	u64 why, what, where;
+	Window win;
+	XEvent ev;
+	XAnyEvent* pev;
+	pev = (void*)&ev;
 
-	where = (u64)this;
+	createmywindow(this);
 	while(1)
 	{
-	XNextEvent(dsp, &ev);
-	if(ev.type==ButtonPress)
-	{
-		//printf("buttonpress\n");
-		if(ev.xbutton.button==Button4)	//front
-		{
-			x = (ev.xbutton.x<<16) / (this->w);
-			y = (ev.xbutton.y<<16) / (this->h);
-			k = 'f';
+		XNextEvent(dsp, &ev);
+		win = pev->window;
+		this = maptab[win&0xfff];
+		where = (u64)this;
 
-			why = x + (y<<16) + (k<<48);
-			what = hex32('p', '+', 0, 0);
-			eventwrite(why, what, where, 0);
-		}
-		else if(ev.xbutton.button==Button5)	//back
+		if(ev.type == ClientMessage)
 		{
-			x = (ev.xbutton.x<<16) / (this->w);
-			y = (ev.xbutton.y<<16) / (this->h);
-			k = 'b';
+			//printf("ClientMessage\n");
+			if(ev.xclient.data.l[0] == wmCreate)
+			{
+				createmywindow((void*)theone + ev.xclient.data.l[1]);
+			}
+			else if(ev.xclient.data.l[0] == wmDelete)
+			{
+				maptab[((u64)win) & 0xfff] = 0;
+				XDestroyWindow(dsp, win);
+				fuckyou--;
 
-			why = x + (y<<16) + (k<<48);
-			what = hex32('p', '+', 0, 0);
+				if(fuckyou <= 0)
+				{
+					eventwrite(0,0,0,0);
+					break;
+				}
+			}
+		}//ClientMessage
+		else if(ev.type == ConfigureNotify)
+		{
+			int x = ev.xconfigure.width;
+			int y = ev.xconfigure.height;
+			//printf("%d,%d\n",x,y);
+
+			if( (x == this->w) && (y == this->h) )continue;
+			this->w = x;
+			this->h = y;
+
+			palette->ximage = (u64)XCreateImage(
+				dsp, visual, 24, ZPixmap,
+				0, (void*)(palette->buf),
+				x,y,32,0
+			);
+
+			why = x + (y<<16);
+			what = hex32('s','i','z','e');
 			eventwrite(why, what, where, 0);
-		}
-/*
-		else if(ev.xbutton.button==Button1)
+		}//ConfigureNotify
+		else if(ev.type == Expose)
 		{
-			this->oldx=ev.xbutton.x;
-			this->oldy=ev.xbutton.y;
-		}
-*/
-		continue;
-	}
-	else if(ev.type==ButtonRelease)
-	{
-		//printf("buttonrelease\n");
-		if(ev.xbutton.button==Button1)
+			XImage* xi;
+			if(palette == 0)continue;
+
+			xi = (void*)(palette->ximage);
+			if(xi == 0)continue;
+
+			XPutImage(
+				dsp, this->fd, (void*)(this->gc), xi,
+				0, 0, 0, 0,
+				this->w, this->h
+			); 
+		}//Expose
+		else if(ev.type == ButtonPress)
 		{
-/*
-		if((this->oldx==ev.xbutton.x)&&
-		   (this->oldy==ev.xbutton.y) )
+			//printf("buttonpress\n");
+			if(ev.xbutton.button == Button4)	//front
+			{
+				x = (ev.xbutton.x<<16) / (this->w);
+				y = (ev.xbutton.y<<16) / (this->h);
+				k = 'f';
+
+				why = x + (y<<16) + (k<<48);
+				what = hex32('p', '+', 0, 0);
+				eventwrite(why, what, where, 0);
+			}
+			else if(ev.xbutton.button == Button5)	//back
+			{
+				x = (ev.xbutton.x<<16) / (this->w);
+				y = (ev.xbutton.y<<16) / (this->h);
+				k = 'b';
+
+				why = x + (y<<16) + (k<<48);
+				what = hex32('p', '+', 0, 0);
+				eventwrite(why, what, where, 0);
+			}
+
+			continue;
+		}//ButtonPress
+		else if(ev.type == ButtonRelease)
+		{
+			if(ev.xbutton.button == Button1)
+			{
+			}
+		}//ButtonRelease
+		else if(ev.type == MotionNotify)
 		{
 			x = (ev.xbutton.x<<16) / (this->w);
 			y = (ev.xbutton.y<<16) / (this->h);
 			k = 'l';
 
 			why = x + (y<<16) + (k<<48);
-			what = hex32('p', '-', 0, 0);
+			what = hex32('p', '@', 0, 0);
 			eventwrite(why, what, where, 0);
-		}
-*/
-		}
-	}
-	else if(ev.type==MotionNotify)
-	{
-/*
-		this->motioncount = (this->motioncount+1)%5;
-		if(this->motioncount != 0)continue;
-*/
-		x = (ev.xbutton.x<<16) / (this->w);
-		y = (ev.xbutton.y<<16) / (this->h);
-		k = 'l';
-
-		why = x + (y<<16) + (k<<48);
-		what = hex32('p', '@', 0, 0);
-		eventwrite(why, what, where, 0);
-/*
-		this->oldx = ev.xbutton.x;
-		this->oldy = ev.xbutton.y;
-*/
-	}
-	else if(ev.type==ClientMessage)
-	{
-		if (ev.xclient.data.l[0] == wmDelete)
+		}//MotionNotify
+		else if(ev.type == KeyPress)
 		{
-			eventwrite(0,0,0,0);
-			break;
-		}
-	}
-	else if(ev.type==ConfigureNotify)
-	{
-		int x = ev.xconfigure.width;
-		int y = ev.xconfigure.height;
-		//printf("%d,%d\n",x,y);
+			//KeyCode keyQ = XKeysymToKeycode(dsp, XStringToKeysym("Q"));
+			//if (ev.xkey.keycode == keyQ)break;
+			//printf("%x\n",ev.xkey.keycode);
 
-		if( (x==this->w) && (y==this->h) )continue;
-		this->w = x;
-		this->h = y;
-
-		palette->ximage = (u64)XCreateImage(
-			dsp, visual, 24, ZPixmap,
-			0, (void*)(palette->buf),
-			x,y,32,0
-		);
-
-		why = x + (y<<16);
-		what = hex32('s','i','z','e');
-		eventwrite(why, what, where, 0);
-	}
-	else if(ev.type==Expose)
-	{
-		XImage* xi;
-		if(palette == 0)continue;
-
-		xi = (void*)(palette->ximage);
-		if(xi == 0)continue;
-
-		XPutImage(
-			dsp, this->fd, (void*)(this->gc), xi,
-			0, 0, 0, 0,
-			this->w, this->h
-		); 
-	}
-	else if(ev.type==KeyPress)
-	{
-		//KeyCode keyQ = XKeysymToKeycode(dsp, XStringToKeysym("Q"));
-		//if (ev.xkey.keycode == keyQ)break;
-		//printf("%x\n",ev.xkey.keycode);
-
-		//普通anscii码
-		what = hex32('c','h','a','r');
-		if((ev.xkey.state&1) == 1)
-		{
-			why = xlib2upper[ev.xkey.keycode];
-			if(why != 0)
+			//普通anscii码
+			what = hex32('c','h','a','r');
+			if((ev.xkey.state&1) == 1)
 			{
-				eventwrite(why, what, where, 0);
-				continue;
+				why = xlib2upper[ev.xkey.keycode];
+				if(why != 0)
+				{
+					eventwrite(why, what, where, 0);
+					continue;
+				}
 			}
-		}
-		else
-		{
-			why = xlib2anscii[ev.xkey.keycode];
-			if(why != 0)
+			else
 			{
-				eventwrite(why, what, where, 0);
-				continue;
+				why = xlib2anscii[ev.xkey.keycode];
+				if(why != 0)
+				{
+					eventwrite(why, what, where, 0);
+					continue;
+				}
 			}
-		}
 
-		//控制按键
-		why = xlib2kbd[ev.xkey.keycode];
-		what = hex32('k','b','d',0);
-		eventwrite(why, what, where, 0);
-	}
+			//控制按键
+			why = xlib2kbd[ev.xkey.keycode];
+			what = hex32('k','b','d',0);
+			eventwrite(why, what, where, 0);
+		}//KeyPress
 	}//while
 
-	//
-	XDestroyWindow(dsp, this->fd);
 	XCloseDisplay(dsp);
 }
 
 
 
 
-void windowwrite(struct window* this)
+void windowwrite(struct window* dst, struct window* src)
 {
 	XEvent ev;
+	u64 fd = dst->fd;
+	if(maptab[fd & 0xfff] == 0)return;
+
 	memset(&ev,0,sizeof(XEvent));
 	ev.type = Expose;
 	ev.xexpose.display = dsp;
-	ev.xexpose.window = this->fd;
-	XSendEvent(dsp, this->fd, False, ExposureMask, &ev);
+	ev.xexpose.window = fd;
+	XSendEvent(dsp, fd, False, ExposureMask, &ev);
 	XFlush(dsp);	//must
+
+	usleep(10000);
 }
 void windowread()
 {
@@ -298,20 +301,35 @@ void windowstart(struct window* this)
 			(void*)(palette->buf), 512, 512,
 			32, 0
 		);
-
-		this->fmt = hex64('b', 'g', 'r', 'a', '8', '8', '8', '8');
 		return;
 	}
 	else
 	{
 		this->type = hex32('w', 'i', 'n', 0);
-                this->fmt = hex32('x', 'l', 'i', 'b');
+		this->fmt = hex64('b', 'g', 'r', 'a', '8', '8', '8', '8');
 
                 for(j=0;j<16;j++)
                 {
                         (this->touch[j]).id = 0xffff;
                 }
-		this->thread = startthread(uievent, this);
+		if(fuckyou == 0)
+		{
+			theone = this;
+			this->thread = startthread(uievent, this);
+		}
+		else
+		{
+			XClientMessageEvent ev;
+			memset(&ev,0,sizeof(XClientMessageEvent));
+			ev.type = ClientMessage;
+			ev.window = root;
+			ev.format = 32;
+			ev.data.l[0] = wmCreate;
+			ev.data.l[1] = (void*)this - (void*)theone;
+			XSendEvent(dsp, theone->fd, False, 0, (void*)&ev);
+			XFlush(dsp);	//must
+			printf("%llx\n",this);
+		}
 	}
 }
 void windowstop()
@@ -319,11 +337,15 @@ void windowstop()
 }
 void windowcreate()
 {
+	int j;
+	for(j=0;j<0x1000;j++)maptab[j] = 0;
+
 	//must
 	XInitThreads();
 
 	//display
 	dsp = XOpenDisplay(NULL);
+	root = DefaultRootWindow(dsp);
 	//int screen = DefaultScreen(dsp);
 
 	//visual
@@ -336,7 +358,8 @@ void windowcreate()
 	}
 
 	//
-	wmDelete=XInternAtom(dsp, "WM_DELETE_WINDOW", True);
+	wmDelete = XInternAtom(dsp, "WM_DELETE_WINDOW", True);
+	wmCreate = XInternAtom(dsp, "WM_CREATE_WINDOW", True);
 }
 void windowdelete()
 {
