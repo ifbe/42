ee68a84abca6e1fa20a2f944bd5da7f0024072d4
ifbe <ifbe@live.cn>
2017-11-16 19:37:16 +0800

    math.complex

--- library/libsoft1/libmath/math.complex.c
+++ library/libsoft1/libmath/math.complex.c
@@ -1,35 +1,78 @@
+double ln(double);
 double squareroot(double);
+double power(double, double);
+double arctan2(double, double);
+double cosine(double);
+double sine(double);
 
 
 
 
-double complex_modulus(double real, double imag)
+float complexmodulus(float* d)
 {
-	return squareroot(real*real + imag*imag);
+	return squareroot(d[0]*d[0] + d[1]*d[1]);
 }
-double complex_angle(double real, double imag)
+float complexangle(float* d)
 {
-	//return atan2(imag, real);
-	return 0.0;
+	return arctan2(d[1], d[0]);
+}
+void complexpower_real(float* d, float e)
+{
+	float mod = complexmodulus(d);
+	float ang = complexangle(d);
+	float ppp = power(mod, e);
+	d[0] = ppp * cosine(ang * e);
+	d[1] = ppp * sine(ang * e);
+}
+void complexpower_complex(float* d, float* s)
+{
+/*
+=(R*exp(i(θ+2kπ))) ^ (a+ib)
+=(exp(i(θ+2kπ)+lnR)) ^ (a+ib)
+=exp((i(θ+2kπ)+lnR) * (a+ib))
+=exp(alnR-(θ+2kπ)*b)*exp(i((θ+2kπ)*a+blnR))
+=exp(alnR-(θ+2kπ)*b)*(cos((θ+2kπ)*a+blnR)+i*sin((θ+2kπ)*a+blnR))
+*/
+	float lnr = 0.5 * ln(d[0]*d[0] + d[1]*d[1]);
+	float ang = arctan2(d[1], d[0]);
+	float j = power(2.71828, s[0]*lnr - s[1]*ang);
+	float k = s[0]*ang + s[1]*lnr;
+	d[0] = j * cosine(k);
+	d[1] = j * sine(k);
 }
 
 
 
 
-void complex_mul(
-	double real1, double imag1,
-	double real2, double imag2,
-	double* r, double* i)
+void complexaddition(float* d, float* s)
 {
-	*r = real1*real2 - imag1*imag2;
-	*i = real1*imag2 + real2*imag1;
+	d[0] += s[0];
+	d[1] += s[1];
 }
-void complex_div(
-	double real1, double imag1,
-	double real2, double imag2,
-	double* r, double* i)
+void complexsubtraction(float* d, float* s)
 {
-	double haha = real2*real2 + imag2*imag2;
-	*r = (real1*real2 + imag1*imag2)/haha;
-	*i = (real2*imag1 - real1*imag2)/haha;
+	d[0] -= s[0];
+	d[1] -= s[1];
+}
+void complexmulply(float* d, float* s)
+{
+	float t[2];
+	t[0] = d[0];
+	t[1] = d[1];
+
+	d[0] = t[0]*s[0] - t[1]*s[1];
+	d[1] = t[0]*s[1] + t[1]*s[0];
+}
+void complexdivision(float* d, float* s)
+{
+	float a;
+	float t[2];
+
+	a = s[0]*s[0] + s[1]*s[1];
+	t[0] = s[0];
+	t[1] = -s[1];
+
+	complexmulply(d, t);
+	d[0] /= a;
+	d[1] /= a;
 }
