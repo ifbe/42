957c435364d7c2cd95e636b077bdefc15572f881
ifbe <ifbe@live.cn>
2017-12-14 15:45:34 +0800

    fix efi: bgr,bgra

--- library/libui0/efi/efi.fb.c
+++ library/libui0/efi/efi.fb.c
@@ -1,6 +1,13 @@
 #include <efi.h>
 #include <efilib.h>
 #include "arena.h"
+#define hex16(a,b) (a | (b<<8))
+#define hex32(a,b,c,d) (a | (b<<8) | (c<<16) | (d<<24))
+#define hex64(a,b,c,d,e,f,g,h) (hex32(a,b,c,d) | (((u64)hex32(e,f,g,h))<<32))
+#define __buf__ hex32('b','u','f',0)
+#define __win__ hex32('w','i','n',0)
+#define __bgra8888__ hex64('r','g','b','a','8','8','8','8')
+#define __bgra8880__ hex64('r','g','b','a','8','8','8','0')
 void gethandleandtable(void* H, void* T);
 void say(void*, ...);
 
@@ -18,12 +25,18 @@ void windowread()
 void windowwrite(struct window* dst, struct window* src)
 {
 	int j;
+	int bpp;
 	u32* ibuf = src->buf;
 	u32* obuf = dst->buf;
+
+	if(dst->fmt == __bgra8888__)bpp = 4;
+	else if(dst->fmt == __bgra8880__)bpp = 3;
+	else return;
+
 	for(j=0;j<1024*768-1;j++)
 	{
 		*obuf = ibuf[j];
-		obuf = (void*)obuf + 3;
+		obuf = (void*)obuf + bpp;
 	}
 }
 void windowlist()
@@ -35,13 +48,15 @@ void windowchoose()
 void windowstart(struct window* this)
 {
 	int ret, num, chosen;
+	u64 fmt;
 	UINTN size = 0;
 	UINTN hlen = 0;
 	EFI_HANDLE* hbuf;
 	EFI_GRAPHICS_OUTPUT_PROTOCOL* gop;
 	EFI_GRAPHICS_OUTPUT_MODE_INFORMATION* info;
+	EFI_PIXEL_BITMASK* pix;
 
-	if(this->type == hex32('b','u','f',0))
+	if(this->type == __buf__)
 	{
 	}
 	else
@@ -92,14 +107,18 @@ void windowstart(struct window* this)
 				gop->Mode->FrameBufferBase,
 				gop->Mode->FrameBufferSize
 			);
-		}
 
-		this->type = hex32('w','i','n',0);
-		this->fmt = hex64('b','g','r','a','8','8','8','8');
-		this->buf = (void*)(gop->Mode->FrameBufferBase);
-		this->len = gop->Mode->FrameBufferSize;
-		this->w = 1024;
-		this->h = 768;
+			pix = &(gop->Mode->Info->PixelInformation);
+			if(pix->ReservedMask == 0xff)fmt = __bgra8888__;
+			else fmt = __bgra8880__;
+
+			this->w = 1024;
+			this->h = 768;
+			this->type = __win__;
+			this->fmt = fmt;
+			this->buf = (void*)(gop->Mode->FrameBufferBase);
+			this->len = gop->Mode->FrameBufferSize;
+		}
 	}
 }
 void windowstop()
