b41d303ab49daa9c96700ec279da91de802a6029
ifbe <ifbe@live.cn>
2017-12-19 19:55:21 +0800

    maze_generate_dfs

--- library/libsoft1/libmem/libgame/solve.maze.c
+++ library/libsoft1/libmem/libgame/solve.maze.c
@@ -43,8 +43,86 @@ void maze_unblock(u8* data, int size, int x, int y, int mask)
 
 
 
-void maze_generate_dfs()
+int maze_available(u8* data, int size, int x, int y)
 {
+	u8 count;
+	u8 asdf[4];
+	u8 this = data[(size*y)+x];
+
+	//random choose one direction
+	count = 0;
+	if((this&0x1) == 0x1)
+	{
+		if(x > 0)
+		{
+		if((data[(size*y)+x-1]&0x80)==0)
+		{
+			asdf[count] = 1;
+			count++;
+		}
+		}
+	}
+	if((this&0x2) == 0x2)
+	{
+		if(x < size-1)
+		{
+		if((data[(size*y)+x+1]&0x80)==0)
+		{
+			asdf[count] = 2;
+			count++;
+		}
+		}
+	}
+	if((this&0x4) == 0x4)
+	{
+		if(y > 0)
+		{
+		if((data[x+(y-1)*size]&0x80)==0)
+		{
+			asdf[count] = 4;
+			count++;
+		}
+		}
+	}
+	if((this&0x8) == 0x8)
+	{
+		if(y < size-1)
+		{
+		if((data[x+(y+1)*size]&0x80)==0)
+		{
+			asdf[count] = 8;
+			count++;
+		}
+		}
+	}
+	if(count == 0)return 0;
+
+	count = getrandom()%count;
+	count = asdf[count];
+	return count;
+}
+void maze_generate_dfs(u8* data, int size, int x, int y)
+{
+	u8* this;
+	u8 wall;
+
+	//visited -> return
+	this = data+(size*y)+x;
+	if((this[0]&0x80) == 0x80)return;
+	this[0] |= 0x80;
+
+	//go into
+	while(1)
+	{
+		wall = maze_available(data, size, x, y);
+		if(wall == 0)break;
+
+		maze_unblock(data, size, x, y, wall);
+		if(wall == 1)maze_generate_dfs(data, size, x-1, y);
+		else if(wall == 2)maze_generate_dfs(data, size, x+1, y);
+		else if(wall == 4)maze_generate_dfs(data, size, x, y-1);
+		else if(wall == 8)maze_generate_dfs(data, size, x, y+1);
+	}
 }
 void maze_generate_kruskal()
 {
@@ -63,11 +141,14 @@ void maze_generate(u8* data, int size)
 			data[y*size+x] = 0xf;
 		}
 	}
+/*
 	maze_unblock(data, size, 0, 0, 1);
 	maze_unblock(data, size, 2, 3, 1);
 	maze_unblock(data, size, 2, 3, 2);
 	maze_unblock(data, size, 2, 3, 4);
 	maze_unblock(data, size, 2, 3, 8);
+*/
+	maze_generate_dfs(data, size, 0, 0);
 }
 void maze_solve()
 {
