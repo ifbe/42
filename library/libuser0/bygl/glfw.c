35a35db8edb5ff0845db9d36330dd3c713f2ea05
ifbe <ifbe@live.cn>
2018-07-30 14:49:55 +0800

    mac fix

--- library/libuser0/bygl/glfw.c
+++ library/libuser0/bygl/glfw.c
@@ -287,11 +287,11 @@ void windowthread()
 		return;
 	}
 
-	glfwWindowHint(GLFW_SAMPLES, 4);	//4x antialiasing
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);	//must 3
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);	//must 2
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);	//MacOS
 
 	rootfunc(root);
 
