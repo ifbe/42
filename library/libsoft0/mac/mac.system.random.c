59ec406120a8068918a6505b139132c2a43e328b
ifbe <ifbe@live.cn>
2018-08-22 15:08:14 +0800

    fix mac: getrandom()

--- library/libsoft0/mac/mac.system.random.c
+++ library/libsoft0/mac/mac.system.random.c
@@ -1,9 +1,33 @@
-void getrandom()
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+int _dev_urandom=-1;
+
+
+
+
+unsigned int getrandom()
 {
+	unsigned int value;
+	int ret=read(_dev_urandom,(char*)&value,4);
+
+	if(ret<4){printf("error reading:/dev/urandom:%x\n",value);}
+	return value;
 }
+
+
+
+
 void createrandom()
 {
+	_dev_urandom=open("/dev/urandom",O_RDONLY);
+	if(_dev_urandom==-1)
+	{
+		printf("error open:/dev/urandom:%x\n",_dev_urandom);
+	}
 }
 void deleterandom()
 {
+	if(_dev_urandom != -1)close(_dev_urandom);
 }
