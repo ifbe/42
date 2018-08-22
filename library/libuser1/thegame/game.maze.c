afb1106e258e6b4f55f242d655a96bd6faf4d26c
ifbe <ifbe@live.cn>
2018-08-22 14:58:36 +0800

    fix maze

--- library/libuser1/thegame/game.maze.c
+++ library/libuser1/thegame/game.maze.c
@@ -7,7 +7,6 @@ void maze_solve(void* buf, int w, int h);
 
 #define WIDTH 32
 #define HEIGHT 32
-static u8 buffer[HEIGHT][WIDTH];
 
 
 
@@ -16,6 +15,7 @@ static void maze_read_pixel(
 	struct arena* win, struct style* sty,
 	struct actor* act, struct pinid* pin)
 {
+	u8* buf;
 	int x,y,w;
 	int cx, cy, ww, hh;
 	if(sty)
@@ -33,11 +33,12 @@ static void maze_read_pixel(
 		hh = win->height/2;
 	}
 
+	buf = act->buf;
 	for(y=0;y<HEIGHT;y++)
 	{
 		for(x=0;x<WIDTH;x++)
 		{
-			w = buffer[y][x];
+			w = buf[WIDTH*y+x];
 			if((w&1) == 1)	//left
 			{
 				drawline(
@@ -143,12 +144,13 @@ static void maze_read_vbo(
 	float* vr = sty->vr;
 	float* vf = sty->vf;
 	float* vu = sty->vu;
+	u8* buf = act->buf;
 
 	for(y=0;y<HEIGHT;y++)
 	{
 		for(x=0;x<WIDTH;x++)
 		{
-			w = buffer[y][x];
+			w = buf[WIDTH*y+x];
 			if((w&1) == 1)	//left
 			{
 				f[0] = (2.0*x+0.0)/WIDTH - 1.0;
@@ -161,10 +163,26 @@ static void maze_read_vbo(
 				tr[1] = vf[1] / HEIGHT;
 				tr[2] = vf[2] / HEIGHT;
 				tf[0] = vu[0] / (WIDTH+HEIGHT);
-				tf[0] = vu[1] / (WIDTH+HEIGHT);
-				tf[0] = vu[2] / (WIDTH+HEIGHT);
+				tf[1] = vu[1] / (WIDTH+HEIGHT);
+				tf[2] = vu[2] / (WIDTH+HEIGHT);
 				carvesolid_rect(win, 0x808080, tc, tr, tf);
 			}
+			else if((w&0x80) == 0x80)
+			{
+				f[0] = (2.0*x+1.0)/WIDTH - 1.0;
+				f[1] = (2.0*y+1.0)/HEIGHT - 1.0;
+				tc[0] = vc[0] + f[0]*vr[0] + f[1]*vf[0];
+				tc[1] = vc[1] + f[0]*vr[1] + f[1]*vf[1];
+				tc[2] = vc[2] + f[0]*vr[2] + f[1]*vf[2];
+				tr[0] = vr[0] / WIDTH/2;
+				tr[1] = vr[1] / WIDTH/2;
+				tr[2] = vr[2] / WIDTH/2;
+				tf[0] = vf[0] / HEIGHT/2;
+				tf[1] = vf[1] / HEIGHT/2;
+				tf[2] = vf[2] / HEIGHT/2;
+				carvesolid_rect(win, 0x808080, tc, tr, tf);
+			}
+
 			if((w&2) == 2)	//right
 			{
 				f[0] = (2.0*x+2.0)/WIDTH - 1.0;
@@ -177,10 +195,26 @@ static void maze_read_vbo(
 				tr[1] = -vf[1] / HEIGHT;
 				tr[2] = -vf[2] / HEIGHT;
 				tf[0] = vu[0] / (WIDTH+HEIGHT);
-				tf[0] = vu[1] / (WIDTH+HEIGHT);
-				tf[0] = vu[2] / (WIDTH+HEIGHT);
+				tf[1] = vu[1] / (WIDTH+HEIGHT);
+				tf[2] = vu[2] / (WIDTH+HEIGHT);
 				carvesolid_rect(win, 0x909090, tc, tr, tf);
 			}
+			else if((w&0x80) == 0x80)
+			{
+				f[0] = (2.0*x+1.0)/WIDTH - 1.0;
+				f[1] = (2.0*y+1.0)/HEIGHT - 1.0;
+				tc[0] = vc[0] + f[0]*vr[0] + f[1]*vf[0];
+				tc[1] = vc[1] + f[0]*vr[1] + f[1]*vf[1];
+				tc[2] = vc[2] + f[0]*vr[2] + f[1]*vf[2];
+				tr[0] = vr[0] / WIDTH/2;
+				tr[1] = vr[1] / WIDTH/2;
+				tr[2] = vr[2] / WIDTH/2;
+				tf[0] = vf[0] / HEIGHT/2;
+				tf[1] = vf[1] / HEIGHT/2;
+				tf[2] = vf[2] / HEIGHT/2;
+				carvesolid_rect(win, 0x808080, tc, tr, tf);
+			}
+
 			if((w&4) == 4)	//down	//careful,different
 			{
 				f[0] = (2.0*x+1.0)/WIDTH - 1.0;
@@ -193,10 +227,26 @@ static void maze_read_vbo(
 				tr[1] = -vr[1] / WIDTH;
 				tr[2] = -vr[2] / WIDTH;
 				tf[0] = vu[0] / (WIDTH+HEIGHT);
-				tf[0] = vu[1] / (WIDTH+HEIGHT);
-				tf[0] = vu[2] / (WIDTH+HEIGHT);
+				tf[1] = vu[1] / (WIDTH+HEIGHT);
+				tf[2] = vu[2] / (WIDTH+HEIGHT);
 				carvesolid_rect(win, 0x707070, tc, tr, tf);
 			}
+			else if((w&0x80) == 0x80)
+			{
+				f[0] = (2.0*x+1.0)/WIDTH - 1.0;
+				f[1] = (2.0*y+1.0)/HEIGHT - 1.0;
+				tc[0] = vc[0] + f[0]*vr[0] + f[1]*vf[0];
+				tc[1] = vc[1] + f[0]*vr[1] + f[1]*vf[1];
+				tc[2] = vc[2] + f[0]*vr[2] + f[1]*vf[2];
+				tr[0] = vr[0] / WIDTH/2;
+				tr[1] = vr[1] / WIDTH/2;
+				tr[2] = vr[2] / WIDTH/2;
+				tf[0] = vf[0] / HEIGHT/2;
+				tf[1] = vf[1] / HEIGHT/2;
+				tf[2] = vf[2] / HEIGHT/2;
+				carvesolid_rect(win, 0x808080, tc, tr, tf);
+			}
+
 			if((w&8) == 8)	//up	//careful,different
 			{
 				f[0] = (2.0*x+1.0)/WIDTH - 1.0;
@@ -209,9 +259,24 @@ static void maze_read_vbo(
 				tr[1] = vr[1] / WIDTH;
 				tr[2] = vr[2] / WIDTH;
 				tf[0] = vu[0] / (WIDTH+HEIGHT);
-				tf[0] = vu[1] / (WIDTH+HEIGHT);
-				tf[0] = vu[2] / (WIDTH+HEIGHT);
+				tf[1] = vu[1] / (WIDTH+HEIGHT);
+				tf[2] = vu[2] / (WIDTH+HEIGHT);
 				carvesolid_rect(win, 0x606060, tc, tr, tf);
+			}
+			else if((w&0x80) == 0x80)
+			{
+				f[0] = (2.0*x+1.0)/WIDTH - 1.0;
+				f[1] = (2.0*y+1.0)/HEIGHT - 1.0;
+				tc[0] = vc[0] + f[0]*vr[0] + f[1]*vf[0];
+				tc[1] = vc[1] + f[0]*vr[1] + f[1]*vf[1];
+				tc[2] = vc[2] + f[0]*vr[2] + f[1]*vf[2];
+				tr[0] = vr[0] / WIDTH/2;
+				tr[1] = vr[1] / WIDTH/2;
+				tr[2] = vr[2] / WIDTH/2;
+				tf[0] = vf[0] / HEIGHT/2;
+				tf[1] = vf[1] / HEIGHT/2;
+				tf[2] = vf[2] / HEIGHT/2;
+				carvesolid_rect(win, 0x808080, tc, tr, tf);
 			}
 		}
 	}
@@ -239,17 +304,18 @@ static void maze_read_tui(
 	struct arena* win, struct style* sty,
 	struct actor* act, struct pinid* pin)
 {
-	int x,y;
-	int stride = win->stride;
-	u8* buf = win->buf;
+	int x,y,stride;
 	u8* p;
+	u8* buf = act->buf;
+	u8* out = win->buf;
 
+	stride = win->stride;
 	for(y=0;y<HEIGHT;y++)
 	{
 		for(x=0;x<WIDTH;x++)
 		{
-			p = buf + (y*stride*4) + (x*8);
-			if(buffer[y][x] != 0)mysnprintf(p, 4, "⬛");
+			p = out + (y*stride*4) + (x*8);
+			if(buf[WIDTH*y+x] != 0)mysnprintf(p, 4, "⬛");
 			else mysnprintf(p, 4, "  ");
 		}
 	}
@@ -259,11 +325,12 @@ static void maze_read_cli(
 	struct actor* act, struct pinid* pin)
 {
 	int x,y;
+	u8* buf = act->buf;
 	for(y=0;y<HEIGHT;y++)
 	{
 		for(x=0;x<WIDTH;x++)
 		{
-			if(buffer[y][x] != 0)say("⬛");	//██
+			if(buf[WIDTH*y+x] != 0)say("⬛");	//██
 			else say("  ");
 		}
 		say("\n");
@@ -303,19 +370,23 @@ static void maze_start(
 	struct arena* win, struct style* sty,
 	struct actor* act, struct pinid* pin)
 {
-	maze_generate(buffer, WIDTH, HEIGHT);
-	maze_solve(buffer, WIDTH, HEIGHT);
+	u8* buf = act->buf;
+	maze_generate(buf, WIDTH, HEIGHT);
+	maze_solve(buf, WIDTH, HEIGHT);
 }
 static void maze_delete(struct actor* act)
 {
 	if(0 == act)return;
-	if(_copy_ == act->type)memorydelete(act->buf);
+	if(act->buf)
+	{
+		memorydelete(act->buf);
+		act->buf = 0;
+	}
 }
 static void maze_create(struct actor* act)
 {
 	if(0 == act)return;
-	if(_orig_ == act->type)act->buf = buffer;
-	if(_copy_ == act->type)act->buf = memorycreate(WIDTH*HEIGHT);
+	act->buf = memorycreate(WIDTH*HEIGHT);
 }
 
 
