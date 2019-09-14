bc45ca487469c8c539c203c3d6342fd54b4e1a5a
ifbe <ifbe@live.cn>
2019-09-14 15:06:12 +0800

    fix rpi: joystick

--- library/libuser0/w2w-gamepad/linux/linux.devjsn.c
+++ library/libuser0/w2w-gamepad/linux/linux.devjsn.c
@@ -23,11 +23,11 @@ void joystick_event(struct arena* win, void* p)
 
 		ev.why = *(u64*)(p+0);
 		ev.what = joy_left;
-		nodetree_rootwrite(win, 0, &ev, 0);
+		relationwrite(win, _dst_, &ev, 0);
 
 		ev.why = *(u64*)(p+8);
 		ev.what = joy_right;
-		nodetree_rootwrite(win, 0, &ev, 0);
+		relationwrite(win, _dst_, &ev, 0);
 	}
 }
 void joystickthread(struct arena* win)
