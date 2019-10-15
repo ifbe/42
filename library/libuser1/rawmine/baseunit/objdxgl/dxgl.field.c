b0c507e4e715be82a584e3a35e2f17232859011f
ifbe <ifbe@live.cn>
2019-10-15 21:24:46 +0800

    little fix

--- library/libuser1/rawmine/baseunit/objdxgl/dxgl.field.c
+++ library/libuser1/rawmine/baseunit/objdxgl/dxgl.field.c
@@ -14,15 +14,17 @@ static void field_draw_vbo3d(
 	struct actor* win, struct style* sty)
 {
 	int x,y,z;
+	float tx,ty,tl;
 	float ax,ay,az;
+	float dx,dy,dz;
 	float* vec;
 
 	vec3 ta,tb;
 	float* vc = sty->f.vc;
 	float* vr = sty->f.vr;
 	float* vf = sty->f.vf;
-	float* vu = sty->f.vt;
-	carveline_prism4(win, 0xffffff, vc, vr, vf, vu);
+	float* vt = sty->f.vt;
+	carveline_prism4(win, 0xffffff, vc, vr, vf, vt);
 
 	vec = act->buf;
 	for(z=0;z<20;z++){
@@ -34,23 +36,37 @@ static void field_draw_vbo3d(
 		ay = y/10.0-0.95;
 		az = z/10.0-0.95;
 
-		ta[0] = vc[0] + vr[0]*ax + vf[0]*ay + vu[0]*az;
-		ta[1] = vc[1] + vr[1]*ax + vf[1]*ay + vu[1]*az;
-		ta[2] = vc[2] + vr[2]*ax + vf[2]*ay + vu[2]*az;
+		ta[0] = vc[0] + vr[0]*ax + vf[0]*ay + vt[0]*az;
+		ta[1] = vc[1] + vr[1]*ax + vf[1]*ay + vt[1]*az;
+		ta[2] = vc[2] + vr[2]*ax + vf[2]*ay + vt[2]*az;
 
-		tb[0] = ta[0] + ta[2] / 10.0;
-		tb[1] = ta[1];
-		tb[2] = ta[2] - ta[0] / 10.0;
+		tx = ta[0] * (1000.0/squareroot(ta[0]*ta[0]+ta[1]*ta[1]) - 1.0);
+		ty = ta[1] * (1000.0/squareroot(ta[0]*ta[0]+ta[1]*ta[1]) - 1.0);
+		tl = squareroot(ta[2]*ta[2] + tx*tx + ty*ty) / 10000.0;
+		dx = tx * tl;
+		dy = ty * tl;
+		dz =-ta[2] * tl;
+		tb[0] = ta[0] + dx - ta[1]/1000;
+		tb[1] = ta[1] + dy + ta[0]/1000;
+		tb[2] = ta[2] + dz;
 
-		carveline_arrow(win, 0x007f7f, ta, tb, vf);
+		carveline_arrow(win, 0x007f7f, ta, tb, vt);
+
 
 		//particle
 		ta[0] = vec[(z*20*20 + y*20 + x)*3 + 0];
 		ta[1] = vec[(z*20*20 + y*20 + x)*3 + 1];
 		ta[2] = vec[(z*20*20 + y*20 + x)*3 + 2];
 
-		vec[(z*20*20 + y*20 + x)*3 + 0] += ta[2] / 100.0 - ta[0]/1000.0;
-		vec[(z*20*20 + y*20 + x)*3 + 2] +=-ta[0] / 100.0 - ta[2]/1000.0;
+		tx = ta[0] * (1000.0/squareroot(ta[0]*ta[0]+ta[1]*ta[1]) - 1.0);
+		ty = ta[1] * (1000.0/squareroot(ta[0]*ta[0]+ta[1]*ta[1]) - 1.0);
+		tl = squareroot(ta[2]*ta[2] + tx*tx + ty*ty) / 100000.0;
+		dx = tx * tl;
+		dy = ty * tl;
+		dz =-ta[2] * tl;
+		vec[(z*20*20 + y*20 + x)*3 + 0] += dx - ta[1]/1000;
+		vec[(z*20*20 + y*20 + x)*3 + 1] += dy + ta[0]/1000;
+		vec[(z*20*20 + y*20 + x)*3 + 2] += dz;
 
 		carvepoint(win, 0xffffff, ta);
 	}
