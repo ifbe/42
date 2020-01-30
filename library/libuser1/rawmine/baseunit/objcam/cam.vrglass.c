b0aeed90935e7dff9599fa56f9402f284558e1b6
ifbe <ifbe@live.cn>
2020-01-30 11:32:57 +0800

    fix: vrglass

--- library/libuser1/rawmine/baseunit/objcam/cam.vrglass.c
+++ library/libuser1/rawmine/baseunit/objcam/cam.vrglass.c
@@ -2,6 +2,7 @@
 #define MATBUF buf0
 #define CAMBUF buf1
 void fixmatrix_transpose(float* m, struct fstyle* sty);
+int gl41data_read(struct halfrel* self, struct halfrel* peer, struct halfrel** stack, int rsp, void* buf, int len);
 
 
 
@@ -11,7 +12,7 @@ static void vrglass_search(struct entity* act, u32 foot, struct halfrel* self[],
 	struct relation* rel;
 	struct entity* world;
 	struct fstyle* obb = 0;
-	//say("freecam@%llx,%llx,%llx,%d\n",act,pin,buf,len);
+	//say("vrglass@%llx,%llx,%llx,%d\n",act,pin,buf,len);
 
 	rel = act->irel0;
 	while(1){
@@ -33,29 +34,31 @@ static void vrglass_delete(struct entity* act)
 }
 static void vrglass_create(struct entity* act, void* str)
 {
-	act->MATBUF = memorycreate(0x1000, 0);
+	act->MATBUF = memorycreate(64*2, 0);
+	act->CAMBUF = memorycreate(0x200, 0);
 }
 
 
 
 
 static int vrglass_draw_gl41(
-	struct entity* act, struct style* pin,
-	struct entity* win, struct style* sty)
+	struct entity* act, struct style* slot,
+	struct entity* scn, struct style* geom,
+	struct entity* wnd, struct style* area)
 {
 	float y;
 	vec3 tc;
-	float* vc = sty->f.vc;
-	float* vr = sty->f.vr;
-	float* vf = sty->f.vf;
-	float* vt = sty->f.vt;
+	float* vc = geom->f.vc;
+	float* vr = geom->f.vr;
+	float* vf = geom->f.vf;
+	float* vt = geom->f.vt;
 
 	for(y=0.0;y<1.01;y+=0.1)
 	{
 		tc[0] = vc[0] + vf[0]*y;
 		tc[1] = vc[1] + vf[1]*y;
 		tc[2] = vc[2] + vf[2]*y;
-		gl41line_rect(win, 0xff0000, tc, vr, vt);
+		gl41line_rect(wnd, 0xff0000, tc, vr, vt);
 	}
 	return 0;
 }/*
@@ -172,10 +175,7 @@ static int vrglass_event111(
 	win->camera.vt[2] = win->height/2 - win->camera.vc[2];
 	return 1;
 }*/
-static int vrglass_event(
-	struct entity* act, struct fstyle* pin,
-	struct entity* win, struct fstyle* sty,
-	struct event* ev, int len)
+static int vrglass_event(struct entity* act, struct fstyle* pin, struct event* ev, int len)
 {
 	short* t;
 	struct fstyle* obb;
@@ -212,20 +212,20 @@ static int vrglass_event(
 
 
 
-void vrglass_frustum(struct fstyle* frus, struct fstyle* obb)
+void vrglass_frustum(struct fstyle* frus, struct fstyle* plane, vec3 eye)
 {
 	float x,y,z,n;
-	//say("@vrglass_frustum\n");
+	//say("@vrglass_frus:%llx,%llx,%llx,%f,%f,%f\n",frus,plane,eye,eye[0],eye[1],eye[2]);
 
-	frus->vc[0] = obb->vq[0];
-	frus->vc[1] = obb->vq[1];
-	frus->vc[2] = obb->vq[2];
+	frus->vc[0] = eye[0];
+	frus->vc[1] = eye[1];
+	frus->vc[2] = eye[2];
 
 
 	//l,r
-	x = obb->vr[0];
-	y = obb->vr[1];
-	z = obb->vr[2];
+	x = plane->vr[0];
+	y = plane->vr[1];
+	z = plane->vr[2];
 	n = squareroot(x*x + y*y + z*z);
 	x /= n;
 	y /= n;
@@ -235,23 +235,23 @@ void vrglass_frustum(struct fstyle* frus, struct fstyle* obb)
 	frus->vl[1] =-y;
 	frus->vl[2] =-z;
 	frus->vl[3] = 
-	  (obb->vc[0] - obb->vr[0] - obb->vq[0])*x
-	+ (obb->vc[1] - obb->vr[1] - obb->vq[1])*y
-	+ (obb->vc[2] - obb->vr[2] - obb->vq[2])*z;
+	  (plane->vc[0] - plane->vr[0] - eye[0])*x
+	+ (plane->vc[1] - plane->vr[1] - eye[1])*y
+	+ (plane->vc[2] - plane->vr[2] - eye[2])*z;
 
 	frus->vr[0] = x;
 	frus->vr[1] = y;
 	frus->vr[2] = z;
 	frus->vr[3] = 
-	  (obb->vc[0] + obb->vr[0] - obb->vq[0])*x
-	+ (obb->vc[1] + obb->vr[1] - obb->vq[1])*y
-	+ (obb->vc[2] + obb->vr[2] - obb->vq[2])*z;
+	  (plane->vc[0] + plane->vr[0] - eye[0])*x
+	+ (plane->vc[1] + plane->vr[1] - eye[1])*y
+	+ (plane->vc[2] + plane->vr[2] - eye[2])*z;
 
 
 	//b,t
-	x = obb->vt[0];
-	y = obb->vt[1];
-	z = obb->vt[2];
+	x = plane->vt[0];
+	y = plane->vt[1];
+	z = plane->vt[2];
 	n = squareroot(x*x + y*y + z*z);
 	x /= n;
 	y /= n;
@@ -261,23 +261,23 @@ void vrglass_frustum(struct fstyle* frus, struct fstyle* obb)
 	frus->vb[1] =-y;
 	frus->vb[2] =-z;
 	frus->vb[3] = 
-	  (obb->vc[0] - obb->vt[0] - obb->vq[0])*x
-	+ (obb->vc[1] - obb->vt[1] - obb->vq[1])*y
-	+ (obb->vc[2] - obb->vt[2] - obb->vq[2])*z;
+	  (plane->vc[0] - plane->vt[0] - eye[0])*x
+	+ (plane->vc[1] - plane->vt[1] - eye[1])*y
+	+ (plane->vc[2] - plane->vt[2] - eye[2])*z;
 
 	frus->vt[0] = x;
 	frus->vt[1] = y;
 	frus->vt[2] = z;
 	frus->vt[3] = 
-	  (obb->vc[0] + obb->vt[0] - obb->vq[0])*x
-	+ (obb->vc[1] + obb->vt[1] - obb->vq[1])*y
-	+ (obb->vc[2] + obb->vt[2] - obb->vq[2])*z;
+	  (plane->vc[0] + plane->vt[0] - eye[0])*x
+	+ (plane->vc[1] + plane->vt[1] - eye[1])*y
+	+ (plane->vc[2] + plane->vt[2] - eye[2])*z;
 
 
 	//n,f
-	x = obb->vf[0];
-	y = obb->vf[1];
-	z = obb->vf[2];
+	x = plane->vf[0];
+	y = plane->vf[1];
+	z = plane->vf[2];
 	n = squareroot(x*x + y*y + z*z);
 	x /= n;
 	y /= n;
@@ -287,53 +287,95 @@ void vrglass_frustum(struct fstyle* frus, struct fstyle* obb)
 	frus->vn[1] = y;
 	frus->vn[2] = z;
 	frus->vn[3] = 
-	  (obb->vc[0] - obb->vq[0])*x
-	+ (obb->vc[1] - obb->vq[1])*y
-	+ (obb->vc[2] - obb->vq[2])*z;
+	  (plane->vc[0] - eye[0])*x
+	+ (plane->vc[1] - eye[1])*y
+	+ (plane->vc[2] - eye[2])*z;
 
 	frus->vf[0] = x;
 	frus->vf[1] = y;
 	frus->vf[2] = z;
 	frus->vf[3] = 1e10;
-/*	say("portal_frustum: (%f,%f), (%f,%f), (%f,%f)\n",
-		frus->vn[3], frus->vf[3], frus->vl[3], frus->vr[3], frus->vb[3], frus->vt[3]);*/
+/*	say("vrglass_frustum: (%f,%f), (%f,%f), (%f,%f), (%f,%f,%f)\n",
+		frus->vn[3], frus->vf[3], frus->vl[3], frus->vr[3], frus->vb[3], frus->vt[3],
+		frus->vc[0], frus->vc[1], frus->vc[2]);*/
 }
 static void vrglass_matrix(
 	struct entity* act, struct style* part,
-	struct entity* wrd, struct style* geom,
-	struct entity* wnd, struct style* area)
+	struct entity* wrd, struct style* geom)
 {
 	struct fstyle* frus = &geom->frus;
 
 	float* mat = act->MATBUF;
 	fixmatrix_transpose((void*)mat, frus);
 	//printmat4(mat);
-
+}
+static void vrglass_camera(
+	struct entity* act, struct style* part,
+	struct entity* wrd, struct style* geom,
+	struct entity* wnd, struct style* area)
+{
+	struct fstyle* frus = &geom->frus;
 	struct glsrc* src = act->CAMBUF;
 	src->arg[0].fmt = 'm';
 	src->arg[0].name = "cammvp";
-	src->arg[0].data = mat;
+	src->arg[0].data = act->MATBUF;
 	src->arg[1].fmt = 'v';
 	src->arg[1].name = "camxyz";
-	src->arg[1].data = frus->vq;
+	src->arg[1].data = frus->vc;
 	wnd->gl_camera[0] = act->CAMBUF;
 }
 
 
 
 
-static void vrglass_read(struct halfrel* self, struct halfrel* peer, void* arg, int idx, void* buf, int len)
+static void vrglass_read_bycam(struct halfrel* self, struct halfrel* peer, struct halfrel** stack, int rsp, void* buf, int len)
 {
+}
+static void vrglass_read_bywnd(struct halfrel* self, struct halfrel* peer, struct halfrel** stack, int rsp, void* buf, int len)
+{
+//say("@vrglass_read_bywnd:%c\n",len);
+//wnd.area -> cam.gl41
+	struct entity* wnd;struct style* area;
+	struct entity* cam;struct style* gl41;
+	wnd = peer->pchip;area = peer->pfoot;
+	cam = self->pchip;gl41 = self->pfoot;
+	vrglass_search(cam, 0, &stack[rsp+0], &stack[rsp+1]);
+
+//cam.slot -> world.geom
+	struct entity* act;struct style* slot;
+	struct entity* wrd;struct style* geom;
+	act = stack[rsp+0]->pchip;slot = stack[rsp+0]->pfoot;
+	wrd = stack[rsp+1]->pchip;geom = stack[rsp+1]->pfoot;
+
+	if('v' == len){
+		vrglass_frustum(&geom->frustum, &geom->fshape, geom->fshape.vq);
+		vrglass_matrix(act,slot, wrd,geom);
+
+		gl41data_read(self, peer, stack, rsp+2, buf, len);
+		vrglass_draw_gl41(act,slot, wrd,geom, wnd,area);
+	}
+	if('?' == len){
+		gl41data_read(self, peer, stack, rsp+2, buf, len);
+		vrglass_camera(act,slot, wrd,geom, wnd,area);
+	}
+//say("@vrglass_read_bywnd.end\n");
+}
+
+
+
+
+static void vrglass_read(struct halfrel* self, struct halfrel* peer, struct halfrel** stack, int rsp, void* buf, int len)
+{
+	struct entity* ent = peer->pchip;
+	switch(ent->fmt){
+		case _gl41fbog_:
+		case _gl41wnd0_:vrglass_read_bywnd(self, peer, stack, rsp, buf, len);break;
+		default:        vrglass_read_bycam(self, peer, stack, rsp, buf, len);break;
+	}
 }
 static int vrglass_write(struct halfrel* self, struct halfrel* peer, void* arg, int idx, void* buf, int len)
 {
-	//if 'ev i' == self.foot
-	struct entity* act = (void*)(self->chip);
-	struct style* pin = (void*)(self->foot);
-	struct entity* win = (void*)(peer->chip);
-	struct style* sty = (void*)(peer->foot);
-	struct event* ev = (void*)buf;
-	vrglass_event(act, &pin->fs, win, &sty->fs, ev, 0);
+	vrglass_event(self->pchip, 0, buf, 0);
 	return 0;
 }
 static void vrglass_discon(struct halfrel* self, struct halfrel* peer)
