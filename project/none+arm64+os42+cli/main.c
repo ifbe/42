5925ad9122acfa272d98696467c8954c79c239ea
ifbe <ifbe@live.cn>
2022-03-28 15:04:23 +0800

    fix makefile for ring3 test app

--- project/none+arm64+os42+cli/main.c
+++ project/none+arm64+os42+cli/main.c
@@ -13,7 +13,7 @@ typedef unsigned long long u64;
 #define _done_ hex32('d','o','n','e')
 #define _take_ hex32('t','a','k','e')
 #define _give_ hex32('g','i','v','e')
-void syscall(u64, u64*);
+void syscall_caller(u64, u64*);
 
 
 
@@ -22,33 +22,33 @@ void main()
 {
 	u64 arg[8];
 	u8 buf[0x1000];
-	syscall(_ver_,   arg);
-	syscall(_yield_, arg);
+	syscall_caller(_ver_,   arg);
+	syscall_caller(_yield_, arg);
 
 	//open
 	arg[0] = (u64)"shit";
 	arg[1] = 0;
-	syscall(_want_, arg);
+	syscall_caller(_want_, arg);
 
 	//read
 /*	arg[0] = ;
 	arg[1] = 0;
 	arg[2] = buf;
 	arg[3] = 0x1000;
-	syscall(_give_, arg);
+	syscall_caller(_give_, arg);
 */
 	//write
 	arg[0] = 1;
 	arg[1] = 0;
 	arg[2] = (u64)"hello world\n";
 	arg[3] = 12;
-	syscall(_give_, arg);
+	syscall_caller(_give_, arg);
 
 	//close
 	arg[0] = 0;
-	syscall(_done_, arg);
+	syscall_caller(_done_, arg);
 
-	syscall(_exit_,  arg);
-	syscall(_ver_,   arg);	//should not print anything
+	syscall_caller(_exit_,  arg);
+	syscall_caller(_ver_,   arg);	//should not print anything
 	for(;;);
 }
--- project/none+x64+os42+cli/main.c
+++ project/none+x64+os42+cli/main.c
@@ -14,7 +14,6 @@ typedef unsigned long long u64;
 #define _take_ hex32('t','a','k','e')
 #define _give_ hex32('g','i','v','e')
 void syscall_caller(u64, u64*);
-#define syscall syscall_caller
 
 
 
@@ -23,33 +22,33 @@ void main()
 {
 	u64 arg[8];
 	u8 buf[0x1000];
-	syscall(_ver_,   arg);
-	syscall(_yield_, arg);
+	syscall_caller(_ver_,   arg);
+	syscall_caller(_yield_, arg);
 
 	//open
 	arg[0] = (u64)"shit";
 	arg[1] = 0;
-	syscall(_want_, arg);
+	syscall_caller(_want_, arg);
 
 	//read
 /*	arg[0] = ;
 	arg[1] = 0;
 	arg[2] = buf;
 	arg[3] = 0x1000;
-	syscall(_give_, arg);
+	syscall_caller(_give_, arg);
 */
 	//write
 	arg[0] = 1;
 	arg[1] = 0;
 	arg[2] = (u64)"hello world\n";
 	arg[3] = 12;
-	syscall(_give_, arg);
+	syscall_caller(_give_, arg);
 
 	//close
 	arg[0] = 0;
-	syscall(_done_, arg);
+	syscall_caller(_done_, arg);
 
-	syscall(_exit_,  arg);
-	syscall(_ver_,   arg);	//should not print anything
+	syscall_caller(_exit_,  arg);
+	syscall_caller(_ver_,   arg);	//should not print anything
 	for(;;);
 }
