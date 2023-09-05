a2f9a25a9819f35689c91a2c6320091e0fb4098a
ifbe <ifbe@live.cn>
2023-09-05 22:10:08 +0800

    reorganize libuser1, modify satellite

--- library/libuser1/reality-manmade/space/space.satellite.c
+++ library/libuser1/reality-manmade/space/space.satellite.c
@@ -2,72 +2,10 @@
 #define longitude whdf.fx0
 #define latitude whdf.fy0
 #define altitude whdf.fz0
-#define CTXBUF listptr.buf0
-void carveplanet(void*, void*, vec3 vc, vec3 vr, vec3 vf, vec3 vu);
-void gl41data_insert(_obj* ctx, int type, struct mysrc* src, int cnt);
 
 
 
 
-char* satellite_glsl_v =
-GLSL_VERSION
-"layout(location = 0)in mediump vec3 vertex;\n"
-"layout(location = 1)in mediump vec2 texuvw;\n"
-"uniform mat4 cammvp;\n"
-"out mediump vec2 uvw;\n"
-"void main(){\n"
-	"uvw = texuvw;\n"
-	"gl_Position = cammvp * vec4(vertex, 1.0);\n"
-"}\n";
-char* satellite_glsl_f =
-GLSL_VERSION
-"in mediump vec2 uvw;\n"
-"out mediump vec4 FragColor;\n"
-"uniform sampler2D tex0;\n"
-"void main(){\n"
-	"FragColor = vec4(texture(tex0, uvw).bgr, 1.0);\n"
-	//"FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
-"}\n";
-
-
-
-
-void satellite_ctxforwnd(struct gl41data* data, char* str)
-{
-	//shader
-	data->src.vs = satellite_glsl_v;
-	data->src.fs = satellite_glsl_f;
-	data->src.shader_enq = 42;
-
-	//texture
-	data->dst.texname[0] = "tex0";
-	data->src.tex[0].fmt = hex32('r','g','b','a');
-	data->src.tex[0].data = memorycreate(2048*2048*4, 0);
-	loadtexfromfile(&data->src.tex[0], str);
-	data->src.tex_enq[0] = 42;
-	//say("w=%d,h=%d\n",data->src.tex[0].w, data->src.tex[0].h);
-
-#define accx 64
-#define accy 63
-	struct vertex* vtx = data->src.vtx;
-	vtx->geometry = 3;
-	vtx->opaque = 0;
-
-	vtx->vbuf_fmt = vbuffmt_33;
-	vtx->vbuf_w = 4*6;
-	vtx->vbuf_h = accx*accy+(accx-1)*2;
-	vtx->vbuf_len = (vtx->vbuf_w) * (vtx->vbuf_h);
-	vtx->vbuf = memorycreate(vtx->vbuf_len, 0);
-
-	vtx->ibuf_fmt = 0x222;
-	vtx->ibuf_w = 2*3;
-	vtx->ibuf_h = accy*(accx-1)*2;
-	vtx->ibuf_len = (vtx->ibuf_w) * (vtx->ibuf_h);
-	vtx->ibuf = memorycreate(vtx->ibuf_len, 0);
-
-	data->src.vbuf_enq = 0;
-	data->src.ibuf_enq = 0;
-}
 static void satellite_draw_gl41(
 	_obj* act, struct style* part,		//self
 	_obj* win, struct style* geom,		//world,mygeom
@@ -77,6 +15,7 @@ static void satellite_draw_gl41(
 	float* vr = geom->fs.vr;
 	float* vf = geom->fs.vf;
 	float* vt = geom->fs.vt;
+	//gl41line_sphere(ctx, 0xffffff, vc,vr,vf,vt);
 
 	vec3 tc,tt,tr,tf;
 	int j;
@@ -89,27 +28,17 @@ static void satellite_draw_gl41(
 	cz = getcos(az);
 	sz = getsin(az);
 	for(j=0;j<3;j++){
-		tt[j] = (vr[j]*c0 + vf[j]*s0)*cz*2 + vt[j]*sz*2;
+		tt[j] = (vr[j]*c0 + vf[j]*s0)*cz + vt[j]*sz;
 		tc[j] = vc[j] + tt[j];
 		tr[j] = -vr[j]*s0 + vf[j]*c0;
 	}
-	vec3_cross(tf, tt, tr);
-	vec3_setlen(tr, 32);
-	vec3_setlen(tf, 32);
 	gl41line(ctx, 0xff00ff, vc, tc);
+
+	float len = vec3_getlen(vr)/100;
+	vec3_cross(tf, tt, tr);
+	vec3_setlen(tr, len);
+	vec3_setlen(tf, len);
 	gl41solid_rect(ctx, 0xffff00, tc, tr, tf);
-
-
-	struct mysrc* src = act->CTXBUF;
-	if(0 == src)return;
-
-	void* vbuf = src->vtx[0].vbuf;
-	void* ibuf = src->vtx[0].ibuf;
-	carveplanet(vbuf, ibuf, vc, vr, vf, vt);
-
-	src->vbuf_enq += 1;
-	src->ibuf_enq += 1;
-	gl41data_insert(ctx, 's', src, 1);
 }
 
 
@@ -227,12 +156,6 @@ static void satellite_create(_obj* act, void* str)
 	act->longitude = 0.0;
 	act->latitude = 0.0;
 	act->altitude = 0.0;
-
-	void* ctx = act->CTXBUF = memorycreate(0x1000, 0);
-	if(0 == ctx)return;
-
-	if(0 == str)str = "datafile/jpg/earth.jpg";
-	satellite_ctxforwnd(ctx, str);
 }
 
 
