179d5cdb5c740baf7a800b4470c35813d512688b
ifbe <ifbe@live.cn>
2024-09-25 00:06:57 +0800

    prepare for bareeficarrybin

--- library/libboot0/bare/efi/carrybin/main.c
+++ library/libboot0/bare/efi/carrybin/main.c
@@ -6,10 +6,9 @@
 extern unsigned char _binary_start_;
 extern unsigned char _binary_end_;
 
-
 EFI_STATUS efi_main(EFI_HANDLE handle, EFI_SYSTEM_TABLE* efitab)
 {
 	int size = _binary_end_ - _binary_start_;
+	efitab->ConOut->OutputString(efitab->ConOut, L"Hello World\r\n");
 	return EFI_SUCCESS;
 }
-//void atexit(){}
